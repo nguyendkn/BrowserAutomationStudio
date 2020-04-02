5d907f7db2149acc4141bced18b18db2d0b28298
CheshireCaat <likangt2012@gmail.com>
2020-04-02 16:46:40 +0300

    Update UI Search
    Remove custom body styles from 'search.css'
    Add 'SearchManager' function
    Move templates to manager
    Add 'Render', 'Search' and 'Recent' functions to SM

--- Solution/ChromeWorker/html/toolbox/search/search.js
+++ Solution/ChromeWorker/html/toolbox/search/search.js
@@ -1,108 +1,68 @@
-let searchTemplate = `
-  <div class='container-fluid mainscreen'>
-    <div class='pagination-container' id='pagination'>
-      <p class='pagination-results' id='count'>0 results</p>
-      <nav>
-        <button class='pagination-button-left pagination-button' data-value='' id='prevpage' aria-label='Previous results'>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 24 24">
-            <line x1="19" y1="12" x2="5" y2="12"></line>
-            <polyline points="12 19 5 12 12 5"></polyline>
-          </svg>
-        </button>
-        <button class='pagination-button-right pagination-button' data-value='' id='nextpage' aria-label='Next results'>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 24 24">
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-            <polyline points="12 5 19 12 12 19"></polyline>
-          </svg>
-        </button>
-      </nav>
-      <p class='pagination-pages'>1 - 9</p>
-    </div>
-    <ol class='results-container' id='results'></ol>
-  </div>
-`;
+function SearchManager() {
+  this.actions = [];
 
-let itemTemplate = _.template(`
-<li class='result-item'>
-    <img src='<%= icon %>'>
-    <div class='hit hit-action' data-value='<%= key %>'>
-      <%= name %>
-    </div>
-    <div class="hit hit-module">
-      <%= groupId %>
-    </div>
-    <div class="hit hit-docs">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </div>
-    <div class="hit hit-docs">
-      {{sub}}
-    </div>
-</li>`);
+  this.Show = function () {
+    $("body").css("overflow-y", "hidden");
+    $(".actions").hide();
+    $(".search").show();
+  };
 
-function BrowserAutomationStudio_InitSearch() {
-  $(".search")
-    .append(searchTemplate)
-    .hide();
+  this.Hide = function () {
+    $("body").css("overfloy-y", "visible");
+    $(".actions").show();
+    $(".search").hide();
+  };
 
-  let keys = Object.keys(_A);
-  let actions = [];
-
-  keys.forEach(key => {
-    let el = _A[key];
-
-    if (el["class"] != "browser") {
-      let groupName = "";
-      let groupId = "";
-
-      if (_A2G[key]) {
-        groupId = _A2G[key];
-        if (_G[groupId]) {
-          groupName = _G[groupId]["name"] + " : ";
-        }
-      }
-
-      if (el["suggestion"]) {
-        actions.push({
-          suggestionEn: el["suggestion"]["en"],
-          suggestionRu: el["suggestion"]["ru"],
-          groupName: groupName,
-          name: tr(el["name"]),
-          groupId: groupId,
-          key: key
-        });
-      } else {
-        actions.push({
-          groupName: groupName,
-          name: tr(el["name"]),
-          groupId: groupId,
-          key: key
-        });
-      }
-    }
-  });
-
-  $("#searchinput").keyup(event => {
+  this.Search = function (query) {
     $("#results").empty();
 
-    let query = $("#searchinput").val();
-    let results = actions.filter(el => {
-      if (el.name.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
-        return true;
-      }
-      return false;
-    });
+    let items = this.actions
+      .filter(el => {
+        if (el.name.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+          return true;
+        }
+        return false;
+      })
+      .map(el => {
+        let queryLower = query.toLowerCase();
+        let nameLower = el.name.toLowerCase();
+        let hStart = nameLower.indexOf(queryLower);
+        let hEnd = hStart + queryLower.length;
+        return {
+          highlightStart: hStart,
+          highlightEnd: hEnd,
+          element: el
+        };
+      });
 
-    console.log(query);
-    console.log(results);
+    this.RenderItems(items);
+  };
 
+  this.Recent = function () {
+    $("#results").empty();
+
+    let items = this.actions
+      .filter(el => ActionHistory.includes(el.key))
+      .map(el => {
+        return {
+          element: el
+        };
+      });
+
+    this.RenderItems(items);
+  }
+
+  this.RenderItems = function (items) {
     let container = $("#results");
     let counter = $("#count");
 
-    results.forEach(val => {
+    items.forEach(result => {
+      let val = result.element;
       if (val.groupId && val.groupId.length > 0) {
         container.append(
-          itemTemplate({
+          this.itemTemplate({
             icon: _G[val.groupId]["icon"],
+            description: val.description,
             groupId: val.groupId,
             name: val.name,
             key: val.key
@@ -110,7 +70,8 @@ function BrowserAutomationStudio_InitSearch() {
         );
       } else {
         container.append(
-          itemTemplate({
+          this.itemTemplate({
+            description: val.description,
             groupId: val.groupId,
             name: val.name,
             key: val.key,
@@ -120,10 +81,95 @@ function BrowserAutomationStudio_InitSearch() {
       }
     });
 
-    $(".hit-action").click(function() {
+    $(".hit-action").click(function () {
       _Router.navigate("#!/" + $(this).data("value"), true);
     });
 
     counter.html(`${results.length} results`);
-  });
+  }
+
+  this.Render = function () {
+    $(".search").append(this.searchTemplate);
+    $(".search").hide();
+
+    this.actions = [];
+
+    Object.keys(_A).forEach(key => {
+      let el = _A[key];
+
+      if (el["class"] != "browser") {
+        let groupName = "";
+        let groupId = "";
+
+        if (_A2G[key]) {
+          groupId = _A2G[key];
+          if (_G[groupId]) {
+            groupName = _G[groupId]["name"] + " : ";
+          }
+        }
+
+        if (el["suggestion"]) {
+          this.actions.push({
+            suggestionEn: el["suggestion"]["en"],
+            suggestionRu: el["suggestion"]["ru"],
+            description: el["description"],
+            groupName: groupName,
+            name: tr(el["name"]),
+            groupId: groupId,
+            key: key
+          });
+        } else {
+          this.actions.push({
+            description: el["description"],
+            groupName: groupName,
+            name: tr(el["name"]),
+            groupId: groupId,
+            key: key
+          });
+        }
+      }
+    });
+  }
+
+  this.Highlight = function (str, element) {
+    
+  }
+
+  this.searchTemplate = `
+  <div class="container-fluid mainscreen">
+    <span class="input-group pagination-container" id="pagination" style="display: none;">
+      <p class="pagination-results" id="count">0 results</p>
+      <nav>
+        <button class="pagination-button-left pagination-button" data-value="" id="prevpage" aria-label="Previous results">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <line x1="19" y1="12" x2="5" y2="12" />
+            <polyline points="12 19 5 12 12 5" />
+          </svg>
+        </button>
+        <button class="pagination-button-right pagination-button" data-value="" id="nextpage" aria-label="Next results">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <line x1="5" y1="12" x2="19" y2="12" />
+            <polyline points="12 5 19 12 12 19" />
+          </svg>
+        </button>
+      </nav>
+      <p class="pagination-pages">1 - 9</p>
+    </span>
+    <ol class="results-container" id="results"></ol>
+  </div>
+`;
+
+  this.itemTemplate = _.template(`
+<li class="result-item">
+    <img src="<%= icon %>">
+    <div class="hit hit-action" data-value="<%= key %>">
+      <%= name %>
+    </div>
+    <div class="hit hit-module">
+      <%= groupId %>
+    </div>
+    <div class="hit hit-docs">
+      <%= description %>
+    </div>
+</li>`);
 }

ecd155de8d53fa86981bceff4c75b30361c64d8d
CheshireCaat <likangt2012@gmail.com>
2020-04-05 02:31:22 +0300

    Update actions documentation (List module)
    Add docs and translations for first ten list actions.

--- Solution/Modules/List/js/delete_by_value_interface.js
+++ Solution/Modules/List/js/delete_by_value_interface.js
@@ -2,4 +2,7 @@
 	<%= _.template($('#variable_constructor').html())({id:"Variable", description:tr("Variable With List"), default_variable: ""}) %>
     <%= _.template($('#input_constructor').html())({id:"Value", description:tr("Value"), default_selector: "string", min_number: -999999, value_number: 0}) %>
 </div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Remove an item with a specific value from the specified list.</div>
+</div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
--- Solution/Modules/List/js/insert_element_to_list_interface.js
+++ Solution/Modules/List/js/insert_element_to_list_interface.js
@@ -3,4 +3,7 @@
     <%= _.template($('#input_constructor').html())({id:"Value", description:tr("Value to add"), default_selector: "string"}) %>
     <%= _.template($('#input_constructor').html())({id:"Index", description:tr("Index"), default_selector: "int", value_number: 0, min_number: -999999}) %>
 </div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Insert an item into the specified list at a specific index.</div>
+</div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

f283f5ac1320a36511411d2f8cfe6b7c6702829b
CheshireCaat <likangt2012@gmail.com>
2020-04-06 18:49:13 +0300

    Update actions documentation (DateTime module)
    Add docs and translations for all DateTime actions.
    Fix intendation in all actions to 4 spaces.
    Manifest refactoring (spaces and semicolons).

--- Solution/Modules/DateTime/js/datetostring_interface.js
+++ Solution/Modules/DateTime/js/datetostring_interface.js
@@ -1,8 +1,10 @@
-<div class="container-fluid httpclientgetheader">
-      <%= _.template($('#input_constructor').html())({id:"String", description:tr("Datetime or string"), default_selector: "string", disable_int:true}) %>
-      <%= _.template($('#input_constructor').html())({id:"Format", description:tr("Datetime format"), default_selector: "string", disable_int:true, value_string: "yyyy-MM-dd", variants: ["yyyy-MM-dd","hh:mm:ss","yyyy-MM-ddThh:mm:ss"]}) %>
-	  <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable To Save"), default_variable: "FORMATTED_DATE"}) %>
-      <a onclick="BrowserAutomationStudio_OpenUrl('http://doc.qt.io/qt-5/qdatetime.html#fromString')" class="tr" style="margin-left: 20px;cursor: pointer;">Formatting dates</a>
-
+<div class="container-fluid">
+    <%= _.template($('#input_constructor').html())({id:"String", description:tr("DateTime or string"), default_selector: "string", disable_int:true}) %>
+    <%= _.template($('#input_constructor').html())({id:"Format", description:tr("DateTime format"), default_selector: "string", disable_int:true, value_string: "yyyy-MM-dd", variants: ["yyyy-MM-dd","hh:mm:ss","yyyy-MM-ddThh:mm:ss"]}) %>
+	<%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable To Save"), default_variable: "FORMATTED_DATE"}) %>
+    <a onclick="BrowserAutomationStudio_OpenUrl('http://doc.qt.io/qt-5/qdatetime.html#fromString')" class="tr" style="margin-left: 20px;cursor: pointer;">DateTime formatting</a>
+</div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Convert the specified date and time to string using a specific format.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
--- Solution/Modules/DateTime/js/millisecond_interface.js
+++ Solution/Modules/DateTime/js/millisecond_interface.js
@@ -1,5 +1,8 @@
-<div class="container-fluid httpclientgetheader">
-      <%= _.template($('#input_constructor').html())({id:"String", description:tr("Datetime or string"), default_selector: "string", disable_int:true}) %>
-	  <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Millisecond"), default_variable: "MILLISECONDS"}) %>
+<div class="container-fluid">
+    <%= _.template($('#input_constructor').html())({id:"String", description:tr("DateTime or string"), default_selector: "string", disable_int:true}) %>
+	<%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Milliseconds"), default_variable: "MILLISECONDS"}) %>
+</div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Get the integer number of milliseconds from the specified date.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

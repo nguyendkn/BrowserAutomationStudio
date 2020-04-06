f5a833e58da79d29de2f25ca07c1f3c6d0e9eff2
CheshireCaat <likangt2012@gmail.com>
2020-04-06 21:40:15 +0300

    Update actions documentation (ImageProcessing module)
    Add docs and translations for all ImageProcessing actions.
    Manifest refactoring (spaces and semicolons).
    Change actions position.

--- Solution/Modules/ImageProcessing/js/imageprocessinggetdata_interface.js
+++ Solution/Modules/ImageProcessing/js/imageprocessinggetdata_interface.js
@@ -1,6 +1,8 @@
-<div class="container-fluid" style="margin-top:30px">
-  <%= _.template($('#input_constructor').html())({id:"Id", description:tr("Image id"), default_selector: "string", disable_int:true}) %>
-  <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable to save image data"), default_variable: "IMAGE_DATA"}) %>
-
+<div class="container-fluid">
+    <%= _.template($('#input_constructor').html())({id:"Id", description:tr("Image id"), default_selector: "string", disable_int:true}) %>
+    <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable to save image data"), default_variable: "IMAGE_DATA"}) %>
+</div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Get the specified image data in base64 format.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
--- Solution/Modules/ImageProcessing/js/imageprocessingload_interface.js
+++ Solution/Modules/ImageProcessing/js/imageprocessingload_interface.js
@@ -1,6 +1,8 @@
-<div class="container-fluid" style="margin-top:30px">
-  <%= _.template($('#input_constructor').html())({id:"Data", description:tr("Image data as base64"), default_selector: "string", disable_int:true}) %>
-  <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable to save image id"), default_variable: "LOADED_IMAGE_ID"}) %>
-
+<div class="container-fluid">
+    <%= _.template($('#input_constructor').html())({id:"Data", description:tr("Image data in base64 format"), default_selector: "string", disable_int:true}) %>
+    <%= _.template($('#variable_constructor').html())({id:"Save", description:tr("Variable to save image id"), default_variable: "LOADED_IMAGE_ID"}) %>
+</div>
+<div class="tooltipinternal">
+    <div class="tr tooltip-paragraph-first-fold">Start working with the specified image and get its id.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

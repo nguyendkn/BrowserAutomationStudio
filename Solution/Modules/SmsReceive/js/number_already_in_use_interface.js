41ec0c2149a0ac72c1718cecbfa19b00d580522f
twaego <mail.to.twaego@gmail.com>
2020-04-08 23:00:33 +0300

    Update translation for sms module

--- Solution/Modules/SmsReceive/js/number_already_in_use_interface.js
+++ Solution/Modules/SmsReceive/js/number_already_in_use_interface.js
@@ -2,6 +2,6 @@
   <%= _.template($('#input_constructor').html())({id:"Number", description:tr("Phone number"), default_selector: "string", disable_int:true}) %>
 </div>
 <div class="tooltipinternal">
-  <div class="tr tooltip-paragraph-first-fold">Send a notification of the used phone number to the service.</div>
+  <div class="tr tooltip-paragraph-first-fold">Send a notification that phone number was already in use on target site.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

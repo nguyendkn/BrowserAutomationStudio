b5e01efab3d982e9a2a07e13864a8ab80bdff10f
twaego <mail.to.twaego@gmail.com>
2020-05-02 18:56:38 +0300

    Fix declaring global variable

--- Solution/ChromeWorker/html/main/xpath_path.js
+++ Solution/ChromeWorker/html/main/xpath_path.js
@@ -17,6 +17,7 @@ _BAS_HIDE(BrowserAutomationStudio_CreateXPathFromElement) = function(elm) {
         } else if (elm.hasAttribute('class')) { 
             segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]'); 
         } else { 
+            var sib;
             for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) { 
                 if (sib.localName == elm.localName)  i++; }; 
                 segs.unshift(elm.localName.toLowerCase() + '[' + i + ']'); 

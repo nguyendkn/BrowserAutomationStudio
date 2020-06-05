2d1fd46c65d38a375c1a7788457cd530228310ba
twaego <mail.to.twaego@gmail.com>
2020-06-05 10:09:41 +0300

    Fingerprint may not be applied instantly after cache update, so need to wait at least one second.

--- Solution/Modules/FingerprintSwitcher/js/apply_fingerprint_code.js
+++ Solution/Modules/FingerprintSwitcher/js/apply_fingerprint_code.js
@@ -1,2 +1,3 @@
 /*Browser*/
 _call(BrowserAutomationStudio_ApplyFingerprint,[<%= fingerprint %>,<%= canvas %> == "true",<%= webgl %> == "true",<%= audio %> == "true",<%= battery %> == "true",<%= rectangles %> == "true"])!
+sleep(1000)!

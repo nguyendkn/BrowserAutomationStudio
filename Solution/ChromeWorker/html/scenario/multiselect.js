347c134f3872aaf39a6073f0e741ba624eed60e8
twaego <mail.to.twaego@gmail.com>
2020-07-02 23:05:06 +0300

    Fixed issue when moving cursor out of window bounds during multiselect(#494)

--- Solution/ChromeWorker/html/scenario/multiselect.js
+++ Solution/ChromeWorker/html/scenario/multiselect.js
@@ -122,6 +122,10 @@ function MultiSelect()
 				y2 = document.documentElement.scrollHeight - 1
 			if(x2 > document.documentElement.scrollWidth - 1)
 				x2 = document.documentElement.scrollWidth - 1
+			if(y2 < 0)
+				y2 = 0
+			if(x2 < 0)
+				x2 = 0
 			if(active)
 			{
 				FindSelectedActions();

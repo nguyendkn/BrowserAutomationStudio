36acf39d59dca12d20e7b21aa28092f5c580ffda
CheshireCaat <likangt2012@gmail.com>
2020-07-06 20:18:51 +0300

    Move video and wiki to separate folder

---
+++ Solution/ChromeWorker/html/toolbox/search/internal/items/video.js
@@ -0,0 +1,523 @@
+_VIDEO = [
+  {
+    url: 'https://www.youtube.com/watch?v=FCfcgDX8ezk',
+    name: 'Proxy checker',
+    suggestion: 'change set proxy proxies ip hide checker loop',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=FCfcgDX8ezk',
+    name: 'Чеккер прокси',
+    suggestion: 'установить сменить поменять прокси ип чеккер чекер циклы цикл',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=lq2Anqlnliw',
+    name: 'Change proxy',
+    suggestion: 'change set proxy proxies ip hide',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=lq2Anqlnliw',
+    name: 'Сменить прокси',
+    suggestion: 'установить сменить поменять прокси ип',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=64He614lmmg',
+    name: 'Solve captcha antigate/dbc',
+    suggestion: 'solve resolve captcha capcha antigate rucaptcha 2captha dbc',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=64He614lmmg',
+    name: 'Решить капчу через антигейт/рукачпу',
+    suggestion: 'решить капчу антигейт рукапча 2капча каптча каптчу',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=7MQFctMCNCw',
+    name: 'Set user agent',
+    suggestion: 'user agent header referrer referer accept language',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=7MQFctMCNCw',
+    name: 'Установить юзер агент',
+    suggestion: 'Юзер агент хедер рефферер реферер',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=qZ4fuY0Uz2E',
+    name: 'Get current browser url',
+    suggestion: 'url address',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=qZ4fuY0Uz2E',
+    name: 'Получить текущий урл браузера',
+    suggestion: 'текущий адрес урл',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=cbppjfetsiU',
+    name: 'Fill form with and without mouse',
+    suggestion: 'type key enter escape',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=cbppjfetsiU',
+    name: 'Заполнить форму без мыши',
+    suggestion: 'ввод клавиша',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=Y5WkLilf5ds',
+    name: 'Execute custom javascript',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=Y5WkLilf5ds',
+    name: 'Выполнить яваскрипт на элементе',
+    suggestion: 'яваскрипт скрипт',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=njXI1bXxlrA',
+    name: 'Scroll page',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=njXI1bXxlrA',
+    name: 'Прокрутка страницы',
+    suggestion: 'скролл',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=3DOzVdGluMY',
+    name: 'Make screnshot',
+    suggestion: 'render page',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=3DOzVdGluMY',
+    name: 'Сделать скриншот',
+    suggestion: 'скрин снимок экран',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=jscgmDpib_U',
+    name: 'Get page from cache',
+    suggestion: 'cache download',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=jscgmDpib_U',
+    name: 'Получить страницу из кеша',
+    suggestion: 'кеша кеш скачать загрузить',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=eNhf3XuZxfY',
+    name: 'Disable images',
+    suggestion: 'restrict images videos',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=eNhf3XuZxfY',
+    name: 'Запретить картинки',
+    suggestion: 'запретить запрещать загрузку изображений',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=3alpI6Gc_yI',
+    name: 'Сheck if url is loaded',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=3alpI6Gc_yI',
+    name: 'Проверить загрузку урл',
+    suggestion: '',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=i8DxUatkqzs',
+    name: 'Set combobox value',
+    suggestion: 'dropdown list',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=i8DxUatkqzs',
+    name: 'Установить значение выпадающего списка',
+    suggestion: 'выпадающий список комбобокс',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=ySSY3KNxAF0',
+    name: 'Check element existence',
+    suggestion: 'visible visibility',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=ySSY3KNxAF0',
+    name: 'Проверить существование элемента',
+    suggestion: 'видимость',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=YF0bgVGWUSI',
+    name: 'Image uploader',
+    suggestion: 'upload file loop iterations',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=YF0bgVGWUSI',
+    name: 'Загрузка изображений',
+    suggestion: 'аплоадер циклы',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=7qciisy0TL0',
+    name: 'Task scheduler',
+    suggestion: 'Task scheduler run launch from command line',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=7qciisy0TL0',
+    name: 'Планировщик заданий',
+    suggestion: 'планировщик заданий запустить командная строка',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=UTrYzXNcb_0',
+    name: 'Disable iframe loader',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=UTrYzXNcb_0',
+    name: 'Запретить загрузку фреймов',
+    suggestion: '',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=U88xUpj2BiI',
+    name: 'Apply regexp to page source',
+    suggestion: 'regular expression regexpr parse parser',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=U88xUpj2BiI',
+    name: 'Применить регулярное выражение к коду страницы',
+    suggestion: 'регулярка парсить парсер',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=nRwkELvb6vE',
+    name: 'Autologin',
+    suggestion: 'cookies cookie save cache',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=nRwkELvb6vE',
+    name: 'Автовход',
+    suggestion: 'куки автологин логин куки кукисы',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=c-K0Ni8gccs',
+    name: 'Write to custom file',
+    suggestion: 'write file choose',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=c-K0Ni8gccs',
+    name: 'Запись в файл выбранный пользователем',
+    suggestion: 'запись файл выбрать открыть файл',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=2vbcN95hynw',
+    name: 'Temp resource',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=2vbcN95hynw',
+    name: 'Временный ресурс',
+    suggestion: '',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=cG_j0QoQfYw',
+    name: 'Construct javascript',
+    suggestion: 'create script expression editor',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=cG_j0QoQfYw',
+    name: 'Конструктор яваскрипт',
+    suggestion: 'создать сконструировать яваскрипт скрипт джаваскрипт',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=6lr07wMrukM',
+    name: 'Use ipv6 proxies',
+    suggestion: 'proxy',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=6lr07wMrukM',
+    name: 'ipv6 прокси',
+    suggestion: 'прокси',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=UBgpWayk0SU',
+    name: 'Variable inspector',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=UBgpWayk0SU',
+    name: 'Инспектор переменных',
+    suggestion: '',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=StmlVY1D5Yg',
+    name: 'Ignore errors',
+    suggestion: 'error resume break',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=StmlVY1D5Yg',
+    name: 'Игнорировать ошибки',
+    suggestion: 'прерывать ошибок ошибки',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=5ktCtp6jsI0',
+    name: 'How to bypass id protection',
+    suggestion: 'css xpath match selectors',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=5ktCtp6jsI0',
+    name: 'Как выбрать элемент со сменным ид',
+    suggestion: 'селектор селекторы',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=UkNvH-QNyxc',
+    name: 'Regular expression constructor',
+    suggestion: 'regexp constructor parsing parser',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=UkNvH-QNyxc',
+    name: 'Конструктор регулярных выражений',
+    suggestion: 'регулярки регулярные выражения выражение конструктор',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=oQfhNRhLd4k',
+    name: 'Drag and drop',
+    suggestion: 'dragging move elements',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=oQfhNRhLd4k',
+    name: 'Перетаскивание',
+    suggestion: 'перетаскивать перетаскивание',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=9wFhvarHRVY',
+    name: 'Xpath parser',
+    suggestion: 'parser xpath html xml',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=9wFhvarHRVY',
+    name: 'Xpath парсер',
+    suggestion: 'парсер хтмл',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=Ofqvf_Rg_7M',
+    name: 'Capmonster image recaptcha',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=Ofqvf_Rg_7M',
+    name: 'Решение рекапча с помощью капмонстр',
+    suggestion: 'Капмонстр рекаптча рекапча',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=sPR6CnzEA4k',
+    name: 'Capmonster audio recaptcha',
+    suggestion: '',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=sPR6CnzEA4k',
+    name: 'Решение рекапча с помощью капмонстр аудио',
+    suggestion: 'Капмонстр рекаптча рекапча',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=jDRFKWUofk0',
+    name: 'Solve recaptcha',
+    suggestion: 'google recaptcha recapcha',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=jDRFKWUofk0',
+    name: 'Решение рекапчи',
+    suggestion: 'Гугл рекаптча рекапча',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=-zhs0oqUwHc',
+    name: 'Change timezone',
+    suggestion: 'date time change proxy',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=-zhs0oqUwHc',
+    name: 'Смена часового пояса',
+    suggestion: 'Изменить часовой пояс прокси',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=E7-Xa2Fl4vs',
+    name: 'Clear evercookies',
+    suggestion: 'clear purge restart reset full remove cookies cookie',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=E7-Xa2Fl4vs',
+    name: 'Полная очистка куки',
+    suggestion:
+          'Очистка куки кукисы перезапуск перезагрузка чистить полная чистка',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=E7-Xa2Fl4vs',
+    name: 'Clear evercookies',
+    suggestion: 'clear purge restart reset full remove cookies cookie',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=E7-Xa2Fl4vs',
+    name: 'Полная очистка куки',
+    suggestion:
+          'Очистка куки кукисы перезапуск перезагрузка чистить полная чистка',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=dkuuHl1LL0U',
+    name: 'Phone activation',
+    suggestion: 'receive sms text message phone number',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=dkuuHl1LL0U',
+    name: 'Активация телефона',
+    suggestion: 'получить смс подтвердить номер телефон телефона',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=EOPqKl9o20o',
+    name: 'Working with frames',
+    suggestion: 'selector frame',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=EOPqKl9o20o',
+    name: 'Работа с фреймами',
+    suggestion: 'селектор селекторы фрейм',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=3AXjUH08qxg',
+    name: 'Cheap solve recaptcha',
+    suggestion: 'google recaptcha recapcha',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=3AXjUH08qxg',
+    name: 'Дешевое решение рекапчи',
+    suggestion: 'Гугл рекаптча рекапча',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=ltWy6O9jsTo',
+    name: 'Solve invisible recaptcha',
+    suggestion: 'google recaptcha recapcha',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=ltWy6O9jsTo',
+    name: 'Решение невидимой рекапчи',
+    suggestion: 'Гугл рекаптча рекапча',
+    lang: 'ru'
+  },
+
+  {
+    url: 'https://www.youtube.com/watch?v=Pal3HsU_UTA',
+    name: 'Change thread number',
+    suggestion: 'thread section function',
+    lang: 'en'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=Pal3HsU_UTA',
+    name: 'Смена количества потоков',
+    suggestion: 'потоков потоки функции',
+    lang: 'ru'
+  }
+];

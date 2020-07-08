bbd23140ab4f4265860b4eac683fe1e13030df97
twaego <mail.to.twaego@gmail.com>
2020-07-08 21:14:52 +0300

    Update ip database format and version

--- Solution/Modules/Timezones/dll/moduledll.cpp
+++ Solution/Modules/Timezones/dll/moduledll.cpp
@@ -7,8 +7,7 @@
 #include <QByteArray>
 #include <QVariant>
 #include <QTimezone>
-#include "GeoIP.h"
-#include "GeoIPCity.h"
+#include "maxminddb.h"
 #include "tmap.h"
 #include "countrytolanguage.h"
 
@@ -16,23 +15,32 @@ extern "C" {
 
     struct GeoDllData
     {
-        GeoIP *gic;
-        GeoIP *gic6;
+        MMDB_s* mmdb;
     };
 
     void* StartDll()
     {
         GeoDllData * data = new GeoDllData();
-        data->gic = GeoIP_open("modules/Timezones/GeoLiteCity.dat", GEOIP_STANDARD);
-        data->gic6 = GeoIP_open("modules/Timezones/GeoLiteCityv6.dat", GEOIP_STANDARD);
+        data->mmdb = new MMDB_s();
+        int status = MMDB_open("modules/Timezones/GeoLite2-City.mmdb", MMDB_MODE_MMAP, data->mmdb);
+        if (MMDB_SUCCESS != status)
+        {
+            MMDB_close(data->mmdb);
+            delete data->mmdb;
+            data->mmdb = 0;
+        }
         return data;
     }
 
     void EndDll(void * DllData)
     {
         GeoDllData * data = (GeoDllData *)DllData;
-        GeoIP_delete(data->gic);
-        GeoIP_delete(data->gic6);
+        if(data->mmdb)
+        {
+            MMDB_close(data->mmdb);
+            delete data->mmdb;
+            data->mmdb = 0;
+        }
         delete data;
     }
 
@@ -46,40 +54,6 @@ extern "C" {
 
     }
 
-    static unsigned long __addr_to_num(const char *addr)
-    {
-        unsigned int c, octet, t;
-        unsigned long ipnum;
-        int i = 3;
-
-        octet = ipnum = 0;
-        while ((c = *addr++)) {
-            if (c == '.') {
-                if (octet > 255) {
-                    return 0;
-                }
-                ipnum <<= 8;
-                ipnum += octet;
-                i--;
-                octet = 0;
-            } else {
-                t = octet;
-                octet <<= 3;
-                octet += t;
-                octet += t;
-                c -= '0';
-                if (c > 9) {
-                    return 0;
-                }
-                octet += c;
-            }
-        }
-        if ((octet > 255) || (i != 0)) {
-            return 0;
-        }
-        ipnum <<= 8;
-        return ipnum + octet;
-    }
 
     void CountryToLanguage(char *InputJson, ResizeFunction AllocateSpace, void* AllocateData, void* DllData, void* ThreadData, unsigned int ThreadId, bool *NeedToStop, bool* WasError)
     {
@@ -150,59 +124,82 @@ extern "C" {
         QVariantMap res;
 
         QString ip = InputJson;
+        ip = ip.replace("[","").replace("]","");
 
-        GeoIPRecord *record;
-        if(ip.startsWith("["))
-        {
-            geoipv6_t Ip = _GeoIP_lookupaddress_v6(ip.replace("[","").replace("]","").toStdString().c_str());
-            record = GeoIP_record_by_ipnum_v6(data->gic6,Ip);
-        }else
-        {
-            unsigned long Ip = __addr_to_num(ip.toStdString().c_str());
-            record = GeoIP_record_by_ipnum(data->gic,Ip);
-        }
-
-        if(record)
-        {
-            res.insert("valid", true);
-            res.insert("city", QString::fromLatin1(record->city));
-            res.insert("country", QString::fromLatin1(record->country_code));
-            res.insert("latitude", record->latitude);
-            res.insert("longitude", record->longitude);
-            int offset = GeoIP_time_zone_offset_by_country_and_region(record->country_code,record->region);
-
-            bool compute = false;
-            if(offset >= 99999)
-            {
-                int id = kdLookupRoot(record->latitude,record->longitude);
-
-                if(id>0)
-                {
-                    const char *region = _timezone_to_region(id);
-                    const char *country = _timezone_to_country(id);
-                    if(region && country)
-                    {
-                        res.insert("timezone", GeoIP_time_zone_by_country_and_region(country,region));
-                        res.insert("offset", GeoIP_time_zone_offset_by_country_and_region(country,region));
-                        res.insert("dstoffset", GeoIP_time_zone_dst_offset_by_country_and_region(country,region));
-                        compute = true;
-                    }
-                }
-            }
-
-            if(!compute)
-            {
-                res.insert("timezone", GeoIP_time_zone_by_country_and_region(record->country_code,record->region));
-                res.insert("offset", offset);
-                res.insert("dstoffset", GeoIP_time_zone_dst_offset_by_country_and_region(record->country_code,record->region));
-            }
-
-            GeoIPRecord_delete(record);
-        }else
+        if(!data->mmdb)
         {
             res.insert("valid", false);
-        }
+        }else
+        {
+            int gai_error, mmdb_error;
+            MMDB_lookup_result_s result = MMDB_lookup_string(data->mmdb, ip.toUtf8().constData(), &gai_error, &mmdb_error);
 
+            if (gai_error != 0 || MMDB_SUCCESS != mmdb_error || !result.found_entry)
+            {
+                res.insert("valid", false);
+            }else
+            {
+
+                /*MMDB_entry_data_list_s *entry_data_list = NULL;
+                int status = MMDB_get_entry_data_list(&result.entry, &entry_data_list);
+                FILE * file = fopen("d:/temp/dump.txt","w");
+                MMDB_dump_entry_data_list(file, entry_data_list, 2);
+                fclose(file);*/
+
+
+
+                MMDB_entry_data_s entry_data_city,entry_data_country,entry_data_latitude,entry_data_longitude,entry_data_timezone;
+                int status_city =  MMDB_get_value(&result.entry, &entry_data_city, "city", "names", "en", NULL);
+                int status_country =  MMDB_get_value(&result.entry, &entry_data_country, "country", "iso_code", NULL);
+                int status_latitude =  MMDB_get_value(&result.entry, &entry_data_latitude, "location", "latitude", NULL);
+                int status_longitude =  MMDB_get_value(&result.entry, &entry_data_longitude, "location", "longitude", NULL);
+                int status_timezone = MMDB_get_value(&result.entry, &entry_data_timezone, "location", "time_zone", NULL);
+
+                if (MMDB_SUCCESS != status_country || !entry_data_country.has_data ||
+                    MMDB_SUCCESS != status_latitude || !entry_data_latitude.has_data ||
+                    MMDB_SUCCESS != status_longitude || !entry_data_longitude.has_data ||
+                    MMDB_SUCCESS != status_timezone || !entry_data_timezone.has_data)
+                {
+                    res.insert("valid", false);
+                }else
+                {
+                    res.insert("valid", true);
+
+                    if(MMDB_SUCCESS == status_city && entry_data_city.has_data)
+                        res.insert("city", QString::fromUtf8(entry_data_city.utf8_string,entry_data_city.data_size));
+                    else
+                        res.insert("city", "");
+
+                    res.insert("country", QString::fromUtf8(entry_data_country.utf8_string,entry_data_country.data_size));
+                    res.insert("latitude", entry_data_latitude.double_value);
+                    res.insert("longitude", entry_data_longitude.double_value);
+                    QString Timezone = QString::fromUtf8(entry_data_timezone.utf8_string,entry_data_timezone.data_size);
+                    res.insert("timezone", Timezone);
+
+                    QDateTime now = QDateTime::currentDateTime();
+                    int offset = - QTimeZone(Timezone.toUtf8()).standardTimeOffset(now) / 60;
+                    int dst = 0;
+
+                    for(int i = 1;i<=12;i++)
+                    {
+                        dst = - QTimeZone(Timezone.toUtf8()).daylightTimeOffset(QDateTime(QDate(now.date().year(),i,1),QTime(0,0))) / 60;
+                        if(dst != 0)
+                            break;
+                    }
+
+
+
+
+                    res.insert("offset", offset);
+                    res.insert("dstoffset", dst + offset);
+                }
+
+
+
+            }
+
+
+        }
 
         QJsonObject object = QJsonObject::fromVariantMap(res);
 
--- Solution/Modules/Timezones/dll/moduledll.pro
+++ Solution/Modules/Timezones/dll/moduledll.pro
@@ -2,21 +2,20 @@ QT       -= gui
 TARGET = moduledll
 TEMPLATE = lib
 SOURCES += moduledll.cpp \
-    GeoIP.c \
-    GeoIP_deprecated.c \
-    GeoIPCity.c \
-    pread.c \
+    data-pool.c \
+    maxminddb.c \
     regionName.c \
     timeZone.c \
     timezoneoffset.c \
     tmap.cpp \
     countrytolanguage.cpp
 HEADERS += moduledll.h \
-    GeoIP.h \
-    GeoIP_internal.h \
-    GeoIPCity.h \
-    pread.h \
+    data-pool.h \
+    maxminddb-compat-util.h \
+    maxminddb.h \
+    maxminddb_config.h \
     tmap.h \
     countrytolanguage.h
+
 LIBS += -lWs2_32
 

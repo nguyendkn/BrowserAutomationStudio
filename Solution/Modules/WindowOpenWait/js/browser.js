c07223a4032fc8267ad02c27baea68b61348bc30
twaego <mail.to.twaego@gmail.com>
2020-07-17 00:58:07 +0300

    Remove WindowOpenWait module

--- Solution/Modules/WindowOpenWait/js/browser.js
+++
@@ -1,9 +0,0 @@
-;_BAS_HIDE(BrowserAutomationStudio_Open) = window.open;
-window.open = function()
-{
-  _BAS_HIDE(BrowserAutomationStudio_Sleep)(100);
-  return _BAS_HIDE(BrowserAutomationStudio_Open).apply(window,arguments);
-};
-
-
-

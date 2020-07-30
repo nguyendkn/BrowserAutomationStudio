e4aeef9e036562256045c6841139af2adcec9c80
CheshireCaat <likangt2012@gmail.com>
2020-07-30 17:12:58 +0300

    Update helper.js
    Add JSDoc for all properties and methods.

--- Solution/ChromeWorker/html/toolbox/search/internal/helper.js
+++ Solution/ChromeWorker/html/toolbox/search/internal/helper.js
@@ -1,16 +1,37 @@
 SearchLib.TextProcessor = {
+  /**
+   * Tokenizer for splitting a string into sentences.
+   */
   sentenceTokenizer: new SearchLib.SentenceTokenizer(),
 
+  /**
+   * Regex pattern for default tokenization.
+   */
   tokenizeRegex: /[^\wА-Яа-яЁё0-9_.]+/,
 
+  /**
+   * Regex pattern for trimming (right).
+   */
   trimRightRegex: /[^\wА-Яа-яЁё]+$/,
 
+  /**
+   * Regex pattern for trimming (left).
+   */
   trimLeftRegex: /^[^\wА-Яа-яЁё]+/,
 
+  /**
+   * Max chars limit for truncating.
+   */
   truncateLimit: 200,
 
-  truncate(line, { matches }) {
-    let { sentence } = this.getSentence(line, matches);
+  /**
+   * Truncate the source text using an array of highlighted keywords and the most relevant sentence.
+   * @param {String[]} keywords - selected highlighted keywords array.
+   * @param {String} string - selected source text string.
+   * @returns {String} truncated text string.
+   */
+  truncate(string, keywords) {
+    let { sentence } = this.getSentence(string, keywords);
     let truncStart = false;
     let truncEnd = false;
     let next = 'start';
@@ -21,8 +42,8 @@ SearchLib.TextProcessor = {
       const head = tokens.shift();
       const last = tokens.pop();
 
-      const skipHead = matches.includes(head);
-      const skipLast = matches.includes(last);
+      const skipHead = keywords.includes(head);
+      const skipLast = keywords.includes(last);
 
       const truncate = (defaultNext) => {
         next = defaultNext || next;
@@ -51,10 +72,18 @@ SearchLib.TextProcessor = {
     return sentence;
   },
 
-  getSentence(line, matches) {
-    const sentences = this.sentenceTokenizer.tokenize(line).map((sentence) => {
+  /**
+   * Get the sentence string from the source text that has the maximum number of highlighted keywords.
+   * @param {String[]} keywords - selected highlighted keywords array.
+   * @param {String} string - selected source text string.
+   * @returns {{sentence: String}} object with sentence.
+   */
+  getSentence(string, keywords) {
+    const sentences = this.sentenceTokenizer.tokenize(string).map((sentence) => {
+      const filtered = keywords.filter((k) => sentence.includes(k));
+
       return {
-        filtered: matches.filter((m) => sentence.includes(m)),
+        filtered: filtered,
         sentence: sentence
       }
     });
@@ -62,18 +91,38 @@ SearchLib.TextProcessor = {
     return _.max(sentences, (v) => v.filtered.length);
   },
 
-  tokenize(string) {
-    return string.split(this.tokenizeRegex).map((token) => this.trim(token));
-  },
-
+  /**
+   * Trim the selected text by removing non-word characters from the right end of the string.
+   * @param {String} string - selected text string.
+   * @returns {String} processed text string.
+   */
   trimRight(string) {
     return string.replace(this.trimRightRegex, '');
   },
 
+  /**
+   * Trim the selected text by removing non-word characters from the left end of the string.
+   * @param {String} string - selected text string.
+   * @returns {String} processed text string.
+   */
   trimLeft(string) {
     return string.replace(this.trimLeftRegex, '');
   },
 
+  /**
+   * Tokenize the selected text by splitting string with pattern.
+   * @param {String} string - selected text string.
+   * @returns {String[]} array of tokens.
+   */
+  tokenize(string) {
+    return string.split(this.tokenizeRegex).map((token) => this.trim(token));
+  },
+
+  /**
+   * Trim the selected text by removing non-word characters.
+   * @param {String} string - selected text string.
+   * @returns {String} processed text string.
+   */
   trim(string) {
     let result = string.slice();
     result = this.trimRight(result);

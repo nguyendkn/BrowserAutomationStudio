552131948fecb26064e2e1c317b0b2e2bb1a1993
CheshireCaat <likangt2012@gmail.com>
2020-08-01 07:56:39 +0300

    Update image processing `Get Pixel` action interface.

--- Solution/Modules/ImageProcessing/js/imageprocessinggetpixel_interface.js
+++ Solution/Modules/ImageProcessing/js/imageprocessinggetpixel_interface.js
@@ -3,8 +3,8 @@
     <%= _.template($('#input_constructor').html())({id:"X", description:tr("X"), default_selector: "int", disable_string:true, value_number:0, min_number:0}) %>
     <%= _.template($('#input_constructor').html())({id:"Y", description:tr("Y"), default_selector: "int", disable_string:true, value_number:0, min_number:0}) %>
     <%= _.template($('#variable_constructor').html())({id:"R", description:tr("Color Red"), default_variable: "PIXEL_R"}) %>
-    <%= _.template($('#variable_constructor').html())({id:"G", description:tr("Color Red"), default_variable: "PIXEL_G"}) %>
-    <%= _.template($('#variable_constructor').html())({id:"B", description:tr("Color Red"), default_variable: "PIXEL_B"}) %>
+    <%= _.template($('#variable_constructor').html())({id:"G", description:tr("Color Green"), default_variable: "PIXEL_G"}) %>
+    <%= _.template($('#variable_constructor').html())({id:"B", description:tr("Color Blue"), default_variable: "PIXEL_B"}) %>
     <%= _.template($('#variable_constructor').html())({id:"A", description:tr("Color Transparency"), default_variable: "PIXEL_A"}) %>
 </div>
 <div class="tooltipinternal">

c2d1242561b55111b75998fd71ff62bf19f998ba
GhostZ <sasha199817@gmail.com>
2020-08-10 14:55:59 +0000

    Fixes and improvements Fix "Upload file/folder" for SFTP and SSH protocols Reworked node.js code for many actions Support for multiple masks through ";" for "Search files/folders"

--- Solution/Modules/FTP/js/FTP_Search_interface.js
+++ Solution/Modules/FTP/js/FTP_Search_interface.js
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 <%= _.template($('#input_constructor').html())({id:"FolderPath", description:tr("Remote directory to search"), default_selector: "string", disable_int:true, value_string: "", help: {description:tr("The path to the directory on the remote server in which the search will be performed."),examples:[{code:"/html/test"},{code:"/public_html"},{code:"/domains/site.com/public_html"}]} }) %>
-<%= _.template($('#input_constructor').html())({id:"Mask", description:tr("File/Folder name mask"), default_selector: "string", disable_int:true, value_string: "*", help: {description: tr("File/Folder name mask for filtering. The final list will include only files/folders whose names match the mask."),examples:[{code:tr("*.js"), description: tr("All JavaScript files")},{code:tr("*.txt"), description: tr("All text files")},{code:tr("*"), description: tr("All files")}]} }) %>
+<%= _.template($('#input_constructor').html())({id:"Mask", description:tr("File/Folder name mask"), default_selector: "string", disable_int:true, value_string: "*", help: {description: tr("File/Folder name mask for filtering. The final list will include only files/folders whose names match the mask."),examples:[{code:tr("*.js"), description: tr("All JavaScript files")},{code:tr("*.txt"), description: tr("All text files")},{code:tr("*.js;*.txt"), description: tr("All JavaScript and text files")},{code:tr("*"), description: tr("All files")}]} }) %>
 <div>
 	<span data-preserve="true" data-preserve-type="check" data-preserve-id="Check">
 	  <input type="checkbox" id="Check" checked/> <label for="Check" class="tr">Search folders</label>  
@@ -18,7 +18,7 @@
 	<div class="tr tooltip-paragraph-first-fold">Search for files/folders in the specified directory on the remote server.</div>
 	<div class="tr tooltip-paragraph-fold">In order to execute this action correctly you need to run "Configure FTP/SSH" action first.</div>
 	<div class="tr tooltip-paragraph-fold">Using a mask, you can search for files/folders with a specific name or extension.</div>
-	<div class="tr tooltip-paragraph-fold">Instead of a mask, you can use a regular expression, for this you need to switch the field type to "expression" and write a regular expression into it.</div>
+	<div class="tr tooltip-paragraph-fold">You can specify several masks by separating them with ";".</div>
 	<div class="tr tooltip-paragraph-fold">If "Search files" option is enabled, the final list will include files.</div>
 	<div class="tr tooltip-paragraph-fold">If "Search folders" option is enabled, the final list will include folders.</div>
 	<div class="tr tooltip-paragraph-fold">If "Search in subfolders" option is enabled, the search will be performed not only in the specified directory, but also recursively in all folders that are in it.</div>

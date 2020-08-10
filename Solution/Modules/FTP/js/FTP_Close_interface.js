48498f9cdfe650cb9a1b405a7bf801db5f515269
GhostZ <sasha199817@gmail.com>
2020-08-10 15:25:52 +0000

    Improved documentation of the "Close connection" action

--- Solution/Modules/FTP/js/FTP_Close_interface.js
+++ Solution/Modules/FTP/js/FTP_Close_interface.js
@@ -1,6 +1,7 @@
 <div class="container-fluid"></div>
 <div class="tooltipinternal">
 	<div class="tr tooltip-paragraph-first-fold">Close FTP/SSH connection.</div>
-	<div class="tr tooltip-paragraph-last-fold">If the connection is already closed or not open yet, this action will do nothing.</div>
+	<div class="tr tooltip-paragraph-fold">If the connection is already closed or not open yet, this action will do nothing.</div>
+	<div class="tr tooltip-paragraph-last-fold">This action is optional because the connection is closed automatically by the timeout specified in the "Configure FTP/SSH" action if it is not used.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

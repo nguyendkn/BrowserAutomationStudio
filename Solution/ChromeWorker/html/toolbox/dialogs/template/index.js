b22fa91b91ded2e3ce3f83625d63590ab2c75094
CheshireCaat <likangt2012@gmail.com>
2020-10-06 13:22:26 +0300

    Update index.js
    Update dialogs template.

--- Solution/ChromeWorker/html/toolbox/dialogs/template/index.js
+++ Solution/ChromeWorker/html/toolbox/dialogs/template/index.js
@@ -26,14 +26,18 @@ BasDialogsLib.template = _.template(`
                 <ul class="modal-list-items">
                   <% map[header].forEach((item) => { %>
                     <li class="modal-list-item" data-id="<%= item.id %>">
-                      <div class="modal-list-text-normal modal-text-nowrap modal-text-<%= metadata.color %>"><%= metadata.template(item) %></div>
+                      <div class="modal-list-text-lg modal-text-nowrap modal-text-<%= metadata.color %>">
+                        <%= metadata.template(item) %>
+                      </div>
                       <% if (item.action) { %>
                         <div class="modal-list-item-content">
                           <svg width="9" height="3" viewBox="0 0 9 3" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="0.5" y="2.5" width="2" height="2" transform="rotate(-90 0.5 2.5)" fill="white" stroke="#bdbdbd" />
                             <rect y="2" width="1" height="9" transform="rotate(-90 0 2)" fill="#bdbdbd" />
                           </svg>    
-                          <div class="modal-list-text-small modal-text-nowrap modal-text-gray"><%= item.action.description %></div>
+                          <div class="modal-list-text-sm modal-text-nowrap modal-text-gray">
+                            <%= item.action.description %>
+                          </div>
                         </div>
                       <% } %>
                     </li>
@@ -43,11 +47,13 @@ BasDialogsLib.template = _.template(`
             </div>
           <% }); %>
         </div>
-        <div class="modal-list-empty"><%= tr('No variables found') %></div>
+        <div id="modalListEmpty"><%= tr('No ' + metadata.pluralName + ' found') %></div>
       </div>
       <div id="modalRecentContainer">
         <div id="modalRecentHeader">
-          <div id="modalRecentHeaderText">Recent <%= metadata.pluralName %>:</div>
+          <div id="modalRecentHeaderText">
+            <%= tr('Recent ' + metadata.pluralName + ':') %>
+          </div>
           <div id="modalRecentHeaderIcon">
             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M11 16L11 1M14.3333 16L2.66667 16C1.74619 16 1 15.2538 1 14.3333L1 2.66667C1 1.74619 1.74619 1 2.66667 1L14.3333 1C15.2538 1 16 1.74619 16 2.66667V14.3333C16 15.2538 15.2538 16 14.3333 16Z" stroke="#4f4f4f" stroke-opacity="0.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -62,15 +68,15 @@ BasDialogsLib.template = _.template(`
         <div id="modalRecentContent">
           <ul id="modalRecentItems">
             <% recent.forEach((item) => { %>
-              <li class="modal-recent-wrap">
-                <div class="modal-recent-item" data-id="<%= items.find(({ name }) => name === item.name).id %>">
-                  <div class="modal-recent-icon-left"></div>
-                  <div class="modal-recent-text modal-text-nowrap modal-text-<%= metadata.color %>"><%= item.name %></div>
-                  <div class="modal-recent-icon-right">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12.5 5.583H15a4.167 4.167 0 010 8.334h-2.5m-5 0H5a4.167 4.167 0 010-8.334h2.5M6.667 9.75h6.666" stroke="#E3E3E3" stroke-opacity=".5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>                
-                  </div>
+              <li class="modal-recent-item" data-id="<%= items.find(({ name }) => name === item.name).id %>">
+                <div class="modal-recent-icon-left"></div>
+                <div class="modal-recent-text modal-text-nowrap modal-text-<%= metadata.color %>">
+                  <%= item.name %>
+                </div>
+                <div class="modal-recent-icon-right">
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.5 5.583H15a4.167 4.167 0 010 8.334h-2.5m-5 0H5a4.167 4.167 0 010-8.334h2.5M6.667 9.75h6.666" stroke="#E3E3E3" stroke-opacity=".5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>                
                 </div>
               </li>
             <% }); %>

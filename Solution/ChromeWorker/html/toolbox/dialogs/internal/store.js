f110e6bd498eba765d8460350168a44d89f5d323
CheshireCaat <likangt2012@gmail.com>
2020-10-16 21:00:04 +0300

    Solve issue #117
    Call `addElements` on the end of `SaveControls` method.
    Use single object as param for `add` store method.
    Update `addResource` store method.
    Update `addFunction` store method.
    Update `addVariable` store method.
    Add `addElements` store method.

--- Solution/ChromeWorker/html/toolbox/dialogs/internal/store.js
+++ Solution/ChromeWorker/html/toolbox/dialogs/internal/store.js
@@ -45,7 +45,7 @@ BasDialogsLib.store = {
    * @param {Object[]} source - source items list.
    * @param {Object[]} target - target items list.
    */
-  add(target, source, collection, predicate) {
+  add({ target, source, collection, predicate, sync = true }) {
     const item = Array.isArray(target)
       ? target.filter((a) => !collection.some(predicate(a))).pop()
       : target;
@@ -60,7 +60,7 @@ BasDialogsLib.store = {
       source.unshift({ ...item, timestamp: (+new Date()) });
     }
 
-    _.remove(source, (src) => !collection.some(predicate(src)));
+    if (sync) _.remove(source, (src) => !collection.some(predicate(src)));
   },
 
   /**
@@ -68,39 +68,63 @@ BasDialogsLib.store = {
    * @param {Boolean} global - add as global variable.
    * @param {Object} target - target variable object.
    */
-  addVariable(target, global) {
+  addVariable(target, global, sync = true) {
     if (!target) return;
 
     if (global) {
       if (!this.globalVariables) { this.globalVariables = []; return; }
-      const predicate = this.predicates.variables;
-      this.add(target, this.globalVariables, _GlobalVariableCollection.toJSON(), predicate);
     } else {
       if (!this.localVariables) { this.localVariables = []; return; }
-      const predicate = this.predicates.variables;
-      this.add(target, this.localVariables, _VariableCollection.toJSON(), predicate);
     }
+
+    this.add({
+      collection: (global ? _GlobalVariableCollection : _VariableCollection).toJSON(),
+      source: (global ? this.globalVariables : this.localVariables),
+      predicate: this.predicates.variables,
+      target,
+      sync,
+    });
   },
 
   /**
    * Add the resource object to the recent resources list.
    * @param {Object} target - target resource object.
    */
-  addResource(target) {
+  addResource(target, sync = true) {
     if (!target) return;
     if (!this.resources) { this.resources = []; return; }
-    const predicate = this.predicates.functions;
-    this.add(target, this.resources, _ResourceCollection.toJSON(), predicate);
+
+    this.add({
+      collection: _ResourceCollection.toJSON(),
+      predicate: this.predicates.resources,
+      source: this.resources,
+      target,
+      sync,
+    });
   },
 
   /**
    * Add the function object to the recent functions list.
    * @param {Object} target - target function object.
    */
-  addFunction(target) {
+  addFunction(target, sync = true) {
     if (!target) return;
     if (!this.functions) { this.functions = []; return; }
-    const predicate = this.predicates.resources;
-    this.add(target, this.functions, _FunctionCollection.toJSON(), predicate);
+
+    this.add({
+      collection: _FunctionCollection.toJSON(),
+      predicate: this.predicates.functions,
+      source: this.functions,
+      target,
+      sync,
+    });
+  },
+
+  addElements(source) {
+    source.forEach((item) => {
+      if (item.type === 'var') {
+        this.addVariable({ name: item.data, global: false }, false, false);
+      }
+    });
   }
 }

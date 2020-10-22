00861ccf547959699fff9c67057ae9a9d8393733
CheshireCaat <likangt2012@gmail.com>
2020-10-22 22:14:23 +0300

    Update library index

--- Solution/ChromeWorker/html/toolbox/dialogs/internal/index.js
+++ Solution/ChromeWorker/html/toolbox/dialogs/internal/index.js
@@ -1,31 +1,7 @@
 window.BasDialogsLib = {
-  initializeDropdowns: () => {
-    $(document).on('show.bs.dropdown', '.input-group', function () {
-      const $parent = $(this), $dropdown = $parent.find('ul[data-toggle="dropdown"]');
+  insertHelper: {},
 
-      if ($dropdown.length) {
-        $dropdown.css('visibility', 'hidden');
-
-        $parent.removeClass('dropup'); const heightTop = visibleHeight($dropdown);
-        $parent.addClass('dropup'); const heightBot = visibleHeight($dropdown);
-        if (heightTop >= heightBot) $parent.removeClass('dropup');
-
-        $dropdown.css('visibility', 'visible');
-      }
-    });
-
-    function visibleHeight($target) {
-      const topScroll = $(window).scrollTop();
-      const botScroll = topScroll + $(window).height();
-
-      const topOffset = $target.offset().top;
-      const botOffset = topOffset + $target.outerHeight();
-
-      const topHeight = Math.max(topScroll, topOffset);
-      const botHeight = Math.min(botScroll, botOffset);
-      return botHeight - topHeight;
-    }
-  },
+  dropdowns: {},
 
   templates: {},
 

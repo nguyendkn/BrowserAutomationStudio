6b51942cc7e0dafbb4fd9946fc3ddaa2842117ee
CheshireCaat <likangt2012@gmail.com>
2020-10-30 15:56:36 +0300

    Error handling for javascript execution.

--- Solution/ChromeWorker/html/toolbox/utils/prepareJavascript.js
+++ Solution/ChromeWorker/html/toolbox/utils/prepareJavascript.js
@@ -15,15 +15,28 @@ function prepareBrowserJavascript(source) {
   return {
     javascriptSource: JSON.stringify(
       `((_BAS_DATA) => {  
-        const _BAS_VARS = JSON.parse(_BAS_DATA); 
-        ${source} 
-        return JSON.stringify(_BAS_VARS); 
+        const _BAS_VARS = JSON.parse(_BAS_DATA);
+        let _BAS_WAS_ERROR = false;
+        let _BAS_LAST_ERROR = '';
+
+        try { 
+          ${source} 
+        } catch (err) {
+          _BAS_LAST_ERROR = err.message;
+          _BAS_WAS_ERROR = true;
+        }
+
+        return JSON.stringify({
+          lastError: _BAS_LAST_ERROR,
+          wasError: _BAS_WAS_ERROR,
+          vars: _BAS_VARS,
+        }); 
       })`)
       .replace(/\s\s+/g, ' ')
       .replace(/\n/g, '')
       .replace(/\r/g, '')
       .concat(' + ')
       .concat(args),
-    javascriptResult: variables.map((name) => `VAR_${name} = result['${name}'];`).join('\n')
+    javascriptResult: variables.map((name) => `VAR_${name} = result["vars"]["${name}"];`).join('\n')
   };
 }

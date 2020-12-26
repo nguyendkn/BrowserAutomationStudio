c71b4c4e7b6a48cc2f6cd61372fc04d827e7b4e7
GhostZ <sasha199817@gmail.com>
2020-12-26 17:52:01 +0000

    Fixes

--- Solution/Modules/String/js/concat_strings_code.js
+++ Solution/Modules/String/js/concat_strings_code.js
@@ -1,1 +1,1 @@
-<%= variable %> = _concat_strings([_avoid_nil(<%= string1 %>), _avoid_nil(<%= string2 %>), _avoid_nil(<%= string3 %>)].concat(_convert_to_arr(_avoid_nil(<%= strings_list %>))),<%= separator %>);
+<%= variable %> = _concat_strings([_avoid_nil(<%= string1 %>), _avoid_nil(<%= string2 %>), _avoid_nil(<%= string3 %>)].concat(_to_arr(_avoid_nil(<%= strings_list %>)) || []), <%= separator %>);
--- Solution/Modules/String/js/create_csv_string_code.js
+++ Solution/Modules/String/js/create_csv_string_code.js
@@ -1,1 +1,1 @@
-<%= variable %> = _csv_generate([_avoid_nil(<%= item1 %>), _avoid_nil(<%= item2 %>), _avoid_nil(<%= item3 %>)].concat(_convert_to_arr(_avoid_nil(<%= items_list %>))),<%= separator %>);
+<%= variable %> = _csv_generate([_avoid_nil(<%= item1 %>), _avoid_nil(<%= item2 %>), _avoid_nil(<%= item3 %>)].concat(_to_arr(_avoid_nil(<%= items_list %>)) || []), <%= separator %>);

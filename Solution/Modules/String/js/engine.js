cd277536139e89d37d333c4968e1cec890cb85bc
GhostZ <sasha199817@gmail.com>
2021-01-24 23:49:55 +0000

    Fix decode in "Punycode encode/decode" action

--- Solution/Modules/String/js/engine.js
+++ Solution/Modules/String/js/engine.js
@@ -1619,6 +1619,60 @@ _punycode = (function(){
 	var error = function(type){
 		fail('_punycode: ' + errors[type][_K]);
 	};
+	if(!String.fromCodePoint){
+		(function(){
+			var defineProperty = (function(){
+				try{
+					var object = {};
+					var $defineProperty = Object.defineProperty;
+					var result = $defineProperty(object, object, object) && $defineProperty;
+				}catch(error){}
+				return result;
+			}());
+			var stringFromCharCode = String.fromCharCode;
+			var floor = Math.floor;
+			var fromCodePoint = function(){
+				var MAX_SIZE = 0x4000;
+				var codeUnits = [];
+				var highSurrogate;
+				var lowSurrogate;
+				var index = -1;
+				var length = arguments.length;
+				if (!length) {
+					return '';
+				}
+				var result = '';
+				while (++index < length) {
+					var codePoint = Number(arguments[index]);
+					if(!isFinite(codePoint) || codePoint < 0 || codePoint > 0x10FFFF || floor(codePoint) != codePoint){
+						throw RangeError('Invalid code point: ' + codePoint);
+					};
+					if (codePoint <= 0xFFFF) {
+						codeUnits.push(codePoint);
+					}else{
+						codePoint -= 0x10000;
+						highSurrogate = (codePoint >> 10) + 0xD800;
+						lowSurrogate = (codePoint % 0x400) + 0xDC00;
+						codeUnits.push(highSurrogate, lowSurrogate);
+					};
+					if(index + 1 == length || codeUnits.length > MAX_SIZE){
+						result += stringFromCharCode.apply(null, codeUnits);
+						codeUnits.length = 0;
+					};
+				};
+				return result;
+			};
+			if(defineProperty){
+				defineProperty(String, 'fromCodePoint', {
+					'value': fromCodePoint,
+					'configurable': true,
+					'writable': true
+				});
+			}else{
+				String.fromCodePoint = fromCodePoint;
+			};
+		}());
+	};
 	var map = function(array, fn){
 		var result = [];
 		var length = array.length;

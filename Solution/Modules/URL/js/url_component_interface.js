f6c88b96d0e8c5239cb2949a210fd5e09c6d27ae
GhostZ <sasha9817@mail.ru>
2021-03-22 20:27:27 +0400

    Fix description

--- Solution/Modules/URL/js/url_component_interface.js
+++ Solution/Modules/URL/js/url_component_interface.js
@@ -23,7 +23,7 @@
 	<div class="tr tooltip-paragraph-first-fold">Encodes or decodes string containing the Url component.</div>
 	<div class="tr tooltip-paragraph-fold">This action can work with in two modes: encode and decode. Modes can be switched with "Encode or decode" parameter. Depending on mode data parameter is encoded string which needs to be decoded, or ordinary string containing the Url component which needs to be encoded.</div>
 	<div class="tr tooltip-paragraph-fold">For example, if the string <code>"?x=test"</code> is specified and encode mode is activated, the action will return <code>"%3Fx%3Dtest"</code>. And if the string <code>"%D1%82%D0%B5%D1%81%D1%82"</code> is specified and decode mode is activated, then the action will return <code>"тест"</code>.</div>
-	<div class="tr tooltip-paragraph-fold">You can get a list of links from a string using the "Extract all links" action.</div>
+	<div class="tr tooltip-paragraph-fold">You can get a list of links from a string using the "Extract all links" action from the "String" module.</div>
 	<div class="tr tooltip-paragraph-last-fold">You can parse a link to items using the "Parse URL" action.</div>
 </div>
 <%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>

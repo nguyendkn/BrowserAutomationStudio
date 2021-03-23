ea07e69468d90089ded9f2d6cf0113ead2577008
CheshireCaat <likangt2012@gmail.com>
2021-03-23 23:25:04 +0300

    Update updater.js

--- Solution/ChromeWorker/html/scenario/updater/updater.js
+++ Solution/ChromeWorker/html/scenario/updater/updater.js
@@ -2,7 +2,12 @@
   window.Scenario.ActionsUpdater = new Backbone.View.extend({
     template: _.template(/*html*/`
       <div class="">
-        <h1>Updater</h1>
+        <select id="actionsUpdaterSelect" class="form-control">
+          <option value="all"><%= tr('All actions in the project') %></option>
+          <option value="current"><%= tr('All actions in the current function') %></option>
+          <option value="selected"><%= tr('Only the selected actions') %></option>
+        </select>
+        <label for="actionsUpdaterSelect">0 <%= tr('actions') %></label>
       </div>
       <div class="actions-updater-footer">
         <button type="button" id="actionsUpdaterCancel" class="btn btn-default standartbutton tr">Cancel</button>
@@ -11,6 +16,10 @@
     `),
 
     className: 'actions-updater',
+
+    updateTargetActions: function () {
+      const target = this.$('#actionsUpdaterSelect').val();
+    },
 
     initialize: function () {
       this.render();
@@ -30,6 +39,10 @@
 
     hide: function () {
       this.$el.hide();
+    },
+
+    events: {
+      'change #actionsUpdaterSelect': 'updateTargetActions'
     }
   })();
 })(window);

4cdaff3729fb599c89d89720d73526b7502752b7
CheshireCaat <likangt2012@gmail.com>
2021-03-24 14:15:42 +0300

    Add `ActionsUpdaterModel` model

--- Solution/ChromeWorker/html/scenario/updater/updater.js
+++ Solution/ChromeWorker/html/scenario/updater/updater.js
@@ -1,5 +1,13 @@
 (function (window) {
-  window.Scenario.ActionsUpdater = new Backbone.View.extend({
+  const ActionsUpdaterModel = Backbone.Model.extend({
+    defaults: {
+      isStarted: false,
+      successCount: 0,
+      errorsCount: 0,
+    }
+  });
+
+  const ActionsUpdaterView = Backbone.View.extend({
     template: _.template(/*html*/`
       <div class="">
         <select id="actionsUpdaterSelect" class="form-control">
@@ -7,7 +15,12 @@
           <option value="current"><%= tr('All actions in the current function') %></option>
           <option value="selected"><%= tr('Only the selected actions') %></option>
         </select>
-        <label for="actionsUpdaterSelect">0 <%= tr('actions') %></label>
+        <label for="actionsUpdaterSelect" id="actionsUpdaterCounter">0 <%= tr('actions') %></label>
+      </div>
+      <div class="">
+        <span><%= tr('successful') %>: <span id="actionsUpdaterSuccessCount">0</span></span>
+        <span><%= tr('unsuccessful') %>: <span id="actionsUpdaterErrorsCount">0</span></span>
+        <div class="actions-updater-progressbar"></div>
       </div>
       <div class="actions-updater-footer">
         <button type="button" id="actionsUpdaterCancel" class="btn btn-default standartbutton tr">Cancel</button>
@@ -17,11 +30,17 @@
 
     className: 'actions-updater',
 
-    updateTargetActions: function () {
-      const target = this.$('#actionsUpdaterSelect').val();
-    },
-
     initialize: function () {
+      this.model = new ActionsUpdaterModel();
+
+      this.model.on('change:successCount', (_, successCount) => {
+        this.$('#actionsUpdaterSuccessCount').text(successCount);
+      });
+
+      this.model.on('change:errorsCount', (_, errorsCount) => {
+        this.$('#actionsUpdaterErrorsCount').text(errorsCount);
+      });
+
       this.render();
     },
 
@@ -43,7 +62,31 @@
     },
 
     events: {
-      'change #actionsUpdaterSelect': 'updateTargetActions'
+      'change #actionsUpdaterSelect': function () {
+        const target = this.$('#actionsUpdaterSelect').val();
+
+        const tasks = _.filter(_TaskCollection.toJSON(), (task, index) => {
+          const id = Number(task['id']);
+
+          if (id !== 0 && !IsFunctionNode(id)) {
+            if (target === 'selected') return task['is_selected'];
+            if (target === 'current') return GetFunctionData(id)['name'] === _GobalModel.get('function_name');
+            return true;
+          }
+
+          return false;
+        });
+
+        this.$('#actionsUpdaterCounter').text(`${tasks.length} ${tr('actions')}`);
+      },
+      'click #actionsUpdaterAccept': function () {
+        this.hide();
+      },
+      'click #actionsUpdaterCancel': function () {
+        this.hide();
+      }
     }
-  })();
+  });
+
+  window.Scenario.ActionsUpdater = new ActionsUpdaterView();
 })(window);

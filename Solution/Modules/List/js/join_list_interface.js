1404105b9c0e0b9f783b1e67382d77e404d4e3bd
GhostZ <sasha9817@mail.ru>
2021-03-28 00:16:12 +0400

    Improved type selection

--- Solution/Modules/List/js/join_list_interface.js
+++ Solution/Modules/List/js/join_list_interface.js
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 	<%= _.template($('#variable_constructor').html())({id:"Variable", description:tr("Variable With List"), default_variable: ""}) %>
-    <%= _.template($('#input_constructor').html())({id:"Separator", description:tr("Separator"), default_selector: "string", variants: ["&#034;\\n&#034;<br/><span style='color:gray;font-size:small'>" + tr("Line break, join into multiline text") + "</span><type>expression","&#034;\\r\\n&#034;<br/><span style='color:gray;font-size:small'>" + tr("Carriage return and line break, join into multiline text") + "</span><type>expression"], disable_int:true, value_string: ","}) %>
+    <%= _.template($('#input_constructor').html())({id:"Separator", description:tr("Separator"), default_selector: "string", variants: [["&#034;\\n&#034;<br/><span style='color:gray;font-size:small'>" + tr("Line break, join into multiline text") + "</span>","expression"],["&#034;\\r\\n&#034;<br/><span style='color:gray;font-size:small'>" + tr("Carriage return and line break, join into multiline text") + "</span>","expression"]], disable_int:true, value_string: ","}) %>
     <%= _.template($('#variable_constructor').html())({id:"VariableResult", description:tr("New List"), default_variable: "JOIN_RESULT_STRING"}) %>
 </div>
 <div class="tooltipinternal">

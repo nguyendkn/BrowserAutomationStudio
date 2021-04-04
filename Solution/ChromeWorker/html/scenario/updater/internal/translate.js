6c03cb176ffd3050afb52ea2af93f77dc04129b7
CheshireCaat <likangt2012@gmail.com>
2021-04-04 18:00:09 +0300

    Add translations for updater

---
+++ Solution/ChromeWorker/html/scenario/updater/internal/translate.js
@@ -0,0 +1,20 @@
+_.extend(_L, {
+  'The same actions may differ in different BAS versions. To update an action to the current BAS version, you need to open and save the action.': {
+    ru: 'В разных версиях BAS одни и те же действия могут отличаться. Чтобы обновить действие на текущую версию BAS необходимо это действие открыть и сохранить.'
+  },
+  'The BAS version of the project is different from the current BAS version, update the project actions?': {
+    ru: 'Версия BAS проекта отличается от текущей версии BAS, обновить действия проекта?'
+  },
+  'This tool allows you to automatically update all the actions in the project.': {
+    ru: 'Этот инструмент позволяет автоматически обновить все действия в проекте.'
+  },
+  'Actions that are not updated may not work correctly or even cause an error.': {
+    ru: 'Не обновленные действия могут работать некорректно или даже вызвать ошибку.'
+  },
+  'You can always open it in the ': {
+    ru: 'Вы всегда можете открыть его в '
+  },
+  'context menu': {
+    ru: 'контекстном меню'
+  }
+});

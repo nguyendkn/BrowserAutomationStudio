d34d6e2fa4ab1cb1e5e446ac3e2dc37e4af33853
CheshireCaat <likangt2012@gmail.com>
2021-04-08 00:39:37 +0300

    Update createCodeEditor.js

--- Solution/ChromeWorker/html/toolbox/utils/createCodeEditor.js
+++ Solution/ChromeWorker/html/toolbox/utils/createCodeEditor.js
@@ -1,23 +1,29 @@
-function createCodeEditor(element, options) {
-  if ($(element).attr('data-installed') !== 'true') {
-    const createEditorHandler = monaco.editor.onDidCreateEditor((editor) => {
-      _MainView.trigger('monacoEditorCreated', editor);
-      createEditorHandler.dispose();
-    });
+(function (global) {
+  const editors = [];
 
-    const createModelHandler = monaco.editor.onDidCreateModel((model) => {
-      _MainView.trigger('monacoModelCreated', model);
-      createModelHandler.dispose();
-    });
+  global.createCodeEditor = function (element, options) {
+    if ($(element).attr('data-installed') !== 'true') {
+      const createEditorHandler = monaco.editor.onDidCreateEditor((editor) => {
+        _MainView.trigger('monacoEditorCreated', editor);
+        createEditorHandler.dispose();
+      });
 
-    const editor = monaco.editor.create(element, _.extend({
-      scrollBeyondLastLine: false,
-      language: 'javascript',
-      automaticLayout: true,
-      fontSize: 12
-    }, options));
+      const createModelHandler = monaco.editor.onDidCreateModel((model) => {
+        _MainView.trigger('monacoModelCreated', model);
+        createModelHandler.dispose();
+      });
 
-    $(element).attr('data-installed', 'true');
-    return editor;
+      const editor = monaco.editor.create(element, _.extend({
+        scrollBeyondLastLine: false,
+        language: 'javascript',
+        automaticLayout: true,
+        fontSize: 12
+      }, options));
+
+      editors.forEach((editor) => editor.dispose());
+      $(element).attr('data-installed', 'true');
+      editors.push(editor);
+      return editor;
+    }
   }
-}
+})(window);

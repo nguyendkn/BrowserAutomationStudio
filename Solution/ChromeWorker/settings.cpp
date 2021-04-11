44a7e9c13ee4b633330f68909fbb4394e593a199
twaego <mail.to.twaego@gmail.com>
2021-04-12 01:03:21 +0300

    Profiles caching

--- Solution/ChromeWorker/settings.cpp
+++ Solution/ChromeWorker/settings.cpp
@@ -37,6 +37,7 @@ void settings::Init()
     restart = true;
     emulate_mouse = true;
     autostart_debug = false;
+    profiles_caching = true;
     debug_toolbox = false;
     debug_scenario = false;
     detector = true;
@@ -136,6 +137,10 @@ void settings::Init()
             if(line.find("AutostartDebug=true") != std::string::npos)
             {
                 autostart_debug = true;
+            }
+            if(line.find("ProfilesCaching=false") != std::string::npos)
+            {
+                profiles_caching = false;
             }
             if(line.find("ToolboxHeight=") != std::string::npos)
             {
@@ -379,6 +384,11 @@ bool settings::AutostartDebug()
     return autostart_debug;
 }
 
+bool settings::ProfilesCaching()
+{
+    return profiles_caching;
+}
+
 int settings::ToolboxHeight()
 {
     return toolbox_height;
@@ -435,6 +445,7 @@ void settings::SaveToFile()
             outfile<<"EnableFlash="<<((use_flash) ? "true" : "false")<<std::endl;
             outfile<<"EnableWidevine="<<((use_widevine) ? "true" : "false")<<std::endl;
             outfile<<"AutostartDebug="<<((autostart_debug) ? "true" : "false")<<std::endl;
+            outfile<<"ProfilesCaching="<<((profiles_caching) ? "true" : "false")<<std::endl;
             outfile<<"ToolboxHeight="<<toolbox_height<<std::endl;
             outfile<<"MaxBrowserStartSimultaneously="<<max_browser_start_simultaneously<<std::endl;
             outfile<<"MinFreeMemoryToStartBrowser="<<min_free_memory_to_start_browser<<std::endl;
@@ -475,6 +486,7 @@ std::string settings::Serialize()
     res["use_flash"] = picojson::value(use_flash);
     res["use_widevine"] = picojson::value(use_widevine);
     res["autostart_debug"] = picojson::value(autostart_debug);
+    res["profiles_caching"] = picojson::value(profiles_caching);
     res["canvas"] = picojson::value(canvas);
     res["canvas_noise"] = picojson::value(canvas_noise);
     res["audio"] = picojson::value(audio);
@@ -514,6 +526,7 @@ void settings::Deserialize(const std::string & Data)
         use_flash = o["use_flash"].get<bool>();
         use_widevine = o["use_widevine"].get<bool>();
         autostart_debug = o["autostart_debug"].get<bool>();
+        profiles_caching = o["profiles_caching"].get<bool>();
 
         canvas = o["canvas"].get<std::string>();
         canvas_noise = o["canvas_noise"].get<std::string>();

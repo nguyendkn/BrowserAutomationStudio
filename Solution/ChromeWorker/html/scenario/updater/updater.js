724b672a8a0f1ba9b7e23c07323c1fc6a95fff32
CheshireCaat <likangt2012@gmail.com>
2021-04-18 02:16:39 +0300

    Update `isUnsuccessfulUpdate` model method

--- Solution/ChromeWorker/html/scenario/updater/updater.js
+++ Solution/ChromeWorker/html/scenario/updater/updater.js
@@ -8,7 +8,7 @@
     },
 
     isUnsuccessfulUpdate() {
-      if (this.get('isStarted')) return;
+      if (this.get('isStarted')) return false;
       const success = this.get('successCount');
       return success !== _.size(this.get('tasks'));
     },

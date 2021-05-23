5d927626d136ded29a8a102c4cf2ec47f4e7c865
CheshireCaat <likangt2012@gmail.com>
2021-05-23 15:35:36 +0300

    Separate task files for different services

---
+++ Solution/Modules/Captcha/js/internal/tasks/funCaptchaTask.antiCaptcha.js
@@ -0,0 +1,19 @@
+(function (tasks, api, _) {
+  const FunCaptchaTask = _.inherit(tasks.AntiCaptchaTask, function (params) {
+    tasks.AntiCaptchaTask.call(this, 'FunCaptcha', {
+      name: 'FunCaptchaTask' + (params.proxy ? '' : 'Proxyless'),
+      rules: {
+        subdomainUrl: { optional: true, name: 'funcaptchaApiJSSubdomain' },
+        publicKey: { name: 'websitePublicKey' },
+        pageUrl: { name: 'websiteURL' },
+        data: { optional: true },
+      },
+      params: params
+    });
+  });
+  FunCaptchaTask.prototype.getSolution = function (response) {
+    return response.solution['token']
+  };
+  api.prototype.FunCaptchaTask = FunCaptchaTask;
+
+})(BASCaptchaSolver.tasks, BASCaptchaSolver.AntiCaptchaApi, BASCaptchaSolver.utils);

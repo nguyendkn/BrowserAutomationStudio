6101ea29a73aba1ac72d932418c90f88c4f3a909
CheshireCaat <likangt2012@gmail.com>
2021-05-28 00:19:39 +0300

    Module refactoring

--- Solution/Modules/Captcha/js/internal/index.js
+++ Solution/Modules/Captcha/js/internal/index.js
@@ -1,38 +1,38 @@
 (function (global) {
-  function CaptchaSolver() {
-    this.helpers = {};
-    this.tasks = {};
-    this.utils = {};
+  global.BASCaptchaSolver = {
+    helpers: {},
+
+    tasks: {},
+
+    utils: {},
+
+    setHelper: function (value) {
+      BASCaptchaSolver.helper = value;
+    },
+
+    setDebug: function (value) {
+      BASCaptchaSolver.debug = value;
+    },
+
+    solveFunCaptcha: function () {
+      BASCaptchaSolver.setHelper(new BASCaptchaSolver.helpers.FunCaptchaHelper());
+      _call_function(BASCaptchaSolver.helper.initialize, _function_arguments())!
+      BASCaptchaSolver.api = BASCaptchaSolver.getServiceApi(_function_arguments());
+      const data = _result_function(); _function_arguments()['pageUrl'] = data.pageUrl;
+
+      _call_function(BASCaptchaSolver.api.solveTask, {
+        taskWaitInterval: _function_argument('taskWaitInterval'),
+        taskWaitDelay: _function_argument('taskWaitDelay'),
+        task: new BASCaptchaSolver.api.FunCaptchaTask({
+          userAgent: _function_argument('userAgent'),
+          pageUrl: _function_argument('pageUrl'),
+          proxy: _function_argument('proxy'),
+          subdomainUrl: data['surl'],
+          publicKey: data['pk'],
+        })
+      })!
+
+      _call_function(BASCaptchaSolver.helper.submitCaptcha, { token: _result_function() })!
+    }
   };
-
-  CaptchaSolver.prototype.setHelper = function (value) {
-    this.helper = value;
-  };
-
-  CaptchaSolver.prototype.setDebug = function (value) {
-    this.debug = value;
-  };
-
-  CaptchaSolver.prototype.solveFunCaptcha = function () {
-    BASCaptchaSolver.setHelper(new BASCaptchaSolver.helpers.FunCaptchaHelper());
-    _call_function(BASCaptchaSolver.helper.initialize, _function_arguments())!
-    BASCaptchaSolver.api = BASCaptchaSolver.getServiceApi(_function_arguments());
-    const data = _result_function(); _function_arguments()['pageUrl'] = data.pageUrl;
-
-    _call_function(BASCaptchaSolver.api.solveTask, {
-      taskWaitInterval: _function_argument('taskWaitInterval'),
-      taskWaitDelay: _function_argument('taskWaitDelay'),
-      task: new BASCaptchaSolver.api.FunCaptchaTask({
-        userAgent: _function_argument('userAgent'),
-        pageUrl: _function_argument('pageUrl'),
-        proxy: _function_argument('proxy'),
-        subdomainUrl: data['surl'],
-        publicKey: data['pk'],
-      })
-    })!
-
-    _call_function(BASCaptchaSolver.helper.submitCaptcha, { token: _result_function() })!
-  };
-
-  global.BASCaptchaSolver = new CaptchaSolver();
 })(this);

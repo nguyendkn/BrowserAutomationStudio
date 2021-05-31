9805d2a109be7d90332a598a4ed16045a97756f5
CheshireCaat <likangt2012@gmail.com>
2021-05-31 22:27:13 +0300

    Update hCaptcha.js

--- Solution/Modules/HCaptcha/js/browser/hCaptcha.js
+++ Solution/Modules/HCaptcha/js/browser/hCaptcha.js
@@ -4,10 +4,10 @@
       _BAS_HIDE(BrowserAutomationStudio_HCaptcha) = value;
       const originalRenderFn = value.render;
 
-      _BAS_HIDE(BrowserAutomationStudio_HCaptcha).render = function (container, opts) {
-        _BAS_HIDE(BrowserAutomationStudio_HCaptchaCallback) = opts.callback;
-        _BAS_HIDE(BrowserAutomationStudio_HCaptchaSitekey) = opts.sitekey;
-        return originalRenderFn(container, opts);
+      _BAS_HIDE(BrowserAutomationStudio_HCaptcha).render = function (container, params) {
+        _BAS_HIDE(BrowserAutomationStudio_HCaptchaCallback) = params.callback;
+        _BAS_HIDE(BrowserAutomationStudio_HCaptchaSitekey) = params.sitekey;
+        return originalRenderFn(container, params);
       };
     },
     get: function () {
@@ -18,8 +18,10 @@
   });
 
   _BAS_HIDE(BrowserAutomationStudio_HCaptchaSolved) = function () {
-    if (typeof (_BAS_HIDE(BrowserAutomationStudio_HCaptchaCallback)) !== 'undefined') {
-      try { _BAS_HIDE(BrowserAutomationStudio_HCaptchaCallback)(); } catch (e) { }
+    let callback = _BAS_HIDE(BrowserAutomationStudio_HCaptchaCallback);
+    if (typeof (callback) !== 'undefined') {
+      if (typeof (callback) === 'string') callback = eval(callback);
+      try { callback(); } catch (e) { }
     }
   };
 })();

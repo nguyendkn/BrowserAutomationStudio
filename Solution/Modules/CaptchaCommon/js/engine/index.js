862ddd785555bd7a8a8038a3ba8f1780f592eaa7
CheshireCaat <likangt2012@gmail.com>
2021-06-03 19:19:41 +0300

    Remove type-specific code from common captcha module

--- Solution/Modules/CaptchaCommon/js/engine/index.js
+++ Solution/Modules/CaptchaCommon/js/engine/index.js
@@ -13,48 +13,5 @@
     setDebug: function (value) {
       BASCaptchaSolver.debug = value;
     },
-
-    solveFunCaptcha: function () {
-      const args = _function_arguments();
-      BASCaptchaSolver.setHelper(new BASCaptchaSolver.helpers.FunCaptchaHelper(args));
-      BASCaptchaSolver.api = BASCaptchaSolver.getService(args);
-      _call_function(BASCaptchaSolver.helper.initialize, {})!
-      const data = _result_function();
-
-      _call_function(BASCaptchaSolver.api.solveTask, {
-        task: new BASCaptchaSolver.api.FunCaptchaTask({
-          subdomainUrl: data.subdomainUrl,
-          publicKey: data.publicKey,
-          userAgent: args.userAgent,
-          pageUrl: data.pageUrl,
-          proxy: args.proxy
-        }),
-        taskWaitInterval: args.taskWaitInterval,
-        taskWaitDelay: args.taskWaitDelay
-      })!
-
-      _call_function(BASCaptchaSolver.helper.submitCaptcha, { token: _result_function() })!
-    },
-
-    solveHCaptcha: function () {
-      const args = _function_arguments();
-      BASCaptchaSolver.setHelper(new BASCaptchaSolver.helpers.HCaptchaHelper(args));
-      BASCaptchaSolver.api = BASCaptchaSolver.getService(args);
-      _call_function(BASCaptchaSolver.helper.initialize, {})!
-      const data = _result_function();
-
-      _call_function(BASCaptchaSolver.api.solveTask, {
-        task: new BASCaptchaSolver.api.HCaptchaTask({
-          userAgent: args.userAgent,
-          pageUrl: data.pageUrl,
-          siteKey: data.siteKey,
-          proxy: args.proxy
-        }),
-        taskWaitInterval: args.taskWaitInterval,
-        taskWaitDelay: args.taskWaitDelay
-      })!
-
-      _call_function(BASCaptchaSolver.helper.submitCaptcha, { token: _result_function() })!
-    },
   };
 })(this);

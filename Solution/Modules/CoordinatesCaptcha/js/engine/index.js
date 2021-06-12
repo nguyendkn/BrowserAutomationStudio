a45b1c87442a8b4853c1271a27615e77b4aff701
CheshireCaat <likangt2012@gmail.com>
2021-06-13 01:36:05 +0300

    Update module

--- Solution/Modules/CoordinatesCaptcha/js/engine/index.js
+++ Solution/Modules/CoordinatesCaptcha/js/engine/index.js
@@ -3,7 +3,7 @@
     const args = _function_arguments();
     BASCaptchaSolver.api = BASCaptchaSolver.getService(args);
 
-    _if(!args.imageData, function () {
+    _if(args.query, function () {
       BASCaptchaSolver.setHelper(new BASCaptchaSolver.helpers.CoordinatesCaptchaHelper(args));
       _call_function(BASCaptchaSolver.helper.initialize, {})!
       args.imageData = _result_function().imageData;
@@ -18,6 +18,8 @@
       waitDelay: args.taskWaitDelay
     })!
 
-    _call_function(BASCaptchaSolver.helper.submitCaptcha, { points: _result_function() })!
+    _if(args.query, function () {
+      _call_function(BASCaptchaSolver.helper.submitCaptcha, { points: _result_function() })!
+    })!
   };
 })(this);

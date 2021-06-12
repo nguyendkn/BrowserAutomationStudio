20fa689fc5cf3326ba0b8eb5abe5085b49073b50
CheshireCaat <likangt2012@gmail.com>
2021-06-12 17:45:11 +0300

    Update coordinatesCaptchaHelper.js

--- Solution/Modules/CoordinatesCaptcha/js/engine/helpers/coordinatesCaptchaHelper.js
+++ Solution/Modules/CoordinatesCaptcha/js/engine/helpers/coordinatesCaptchaHelper.js
@@ -13,8 +13,14 @@
     const self = this; _call_function(self.ensureSelector, {})!
 
     _if_else(_result_function(), function () {
-
-    }, function () { fail(tr("Failed to find an element with the specified selector for CoordinatesCaptcha solving")) })!
+      _call_function(self.extractData, {})!
+    }, function () {
+      if (_K === 'en') {
+        fail("Failed to find an element with the specified selector for CoordinatesCaptcha solving");
+      } else {
+        fail("Не удалось найти элемент с указанным селектором для решения CoordinatesCaptcha");
+      }
+    })!
   };
 
   solver.helpers.CoordinatesCaptchaHelper = CoordinatesCaptchaHelper;

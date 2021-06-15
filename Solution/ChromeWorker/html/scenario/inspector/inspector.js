d11893b4ccd1c6c01ac32a89ddf3a18c8289855b
CheshireCaat <likangt2012@gmail.com>
2021-06-15 15:06:23 +0300

    Update

--- Solution/ChromeWorker/html/scenario/inspector/inspector.js
+++ Solution/ChromeWorker/html/scenario/inspector/inspector.js
@@ -19,20 +19,22 @@
   const InspectorView = Backbone.View.extend({
     template: _.template(/*html*/`
       <div style="position:absolute;top:9px;right:30px">
-        <a href="#" id="closevariableinspector" class="text-danger"><i class="fa fa-times-circle-o" aria-hidden="true" style="font-size: 150%;background-color: #fafafa;padding: 5px;"></i></a>
+        <a href="#" id="closeVariableInspector" class="text-danger">
+          <i class="fa fa-times-circle-o" aria-hidden="true" style="font-size: 150%;background-color: #fafafa;padding: 5px;"></i>
+        </a>
       </div>
-      <div id="pendingnotice" style="<%= (global['show_variable_inspector_pending']) ? '' : 'display:none' %>">
+      <div id="pendingNotice" style="<%= (global['show_variable_inspector_pending']) ? '' : 'display:none' %>">
         <span><%= tr("Variables will be loaded on next script pause") %></span>
       </div>
-      <div id="variablelistpending" style="<%= (global['show_variable_inspector_pending']) ? 'display:none' : '' %>">
+      <div id="variableListPending" style="<%= (global['show_variable_inspector_pending']) ? 'display:none' : '' %>">
         <% const data = JSON.parse(global['variable_inspector_data']) %>
-        <div class='variableslabelcontainer'>
-          <span class='variableslabel'><%= tr('Variables:') %></span>
+        <div class="variables-label-container">
+          <span class="variables-label"><%= tr('Variables:') %></span>
         </div>
         <div id="inspectorVariablesData"><%= JSONTree.create(data[0]) %></div>
 
-        <div class='variableslabelcontainer'>
-          <span class='variableslabel'><%= tr('Resources:') %></span>
+        <div class="variables-label-container">
+          <span class="variables-label"><%= tr('Resources:') %></span>
         </div>
         <div id="inspectorResourcesData"><%= JSONTree.create(data[1]) %></div>
       </div>
@@ -67,6 +69,15 @@
       }
       return this;
     },
+
+    events: {
+      'click #closeVariableInspector': function (event) {
+        event.preventDefault();
+        _MainView.model.set('show_variable_inspector', false);
+        $('#variableInspector').hide();
+        $('.main').css('padding-bottom', '50px');
+      },
+    }
   });
 
   global.Scenario.Inspector = InspectorView;

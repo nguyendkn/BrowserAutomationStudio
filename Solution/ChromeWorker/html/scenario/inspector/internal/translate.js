89ed66501c7d149b25c771674cedc1e209ffd4d2
CheshireCaat <likangt2012@gmail.com>
2021-06-18 17:17:51 +0300

    Update localization

--- Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
@@ -2,13 +2,16 @@ _.extend(_L, {
   'Variables will be loaded on next script pause': {
     ru: 'Переменные будут загружены при следующей паузе сценария'
   },
+  'No variables:': {
+    ru: 'Нет переменных'
+  },
+  'No resources:': {
+    ru: 'Нет ресурсов'
+  },
   'Variables:': {
     ru: 'Переменные:'
   },
   'Resources:': {
     ru: 'Ресурсы:'
-  },
-  'No data': {
-    ru: 'Нет данных'
   },
 });

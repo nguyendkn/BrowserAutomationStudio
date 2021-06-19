054ba53dcb09b4f2d4a417731d5341b92b393eb6
CheshireCaat <likangt2012@gmail.com>
2021-06-19 13:11:01 +0300

    Use `Math.max` and `Math.min` in order to limit inspector height

--- Solution/ChromeWorker/html/scenario/inspector/inspector.js
+++ Solution/ChromeWorker/html/scenario/inspector/inspector.js
@@ -137,8 +137,8 @@
             const functionsHeight = $('#functions').outerHeight();
 
             let height = windowHeight - event.client.y - functionsHeight;
-            height = Math.max(height, windowHeight - functionsHeight - 300);
-            height = Math.min(height, 100);
+            height = Math.min(height, windowHeight - functionsHeight - 300);
+            height = Math.max(height, 100);
             this.$el.css('height', height + 'px');
 
             $('.main').css('padding-bottom', (50 + height).toString() + 'px');

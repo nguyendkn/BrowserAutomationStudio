cd949c0b0ad2c271957c4c59a0b001ac73ab7cac
CheshireCaat <likangt2012@gmail.com>
2021-07-04 14:53:17 +0300

    Update jsontree.js

--- Solution/ChromeWorker/html/scenario/jsontree.js
+++ Solution/ChromeWorker/html/scenario/jsontree.js
@@ -21,15 +21,11 @@
       this.root = '';
 
       if (isArray(data)) {
-        console.time('Render JSONTree array root');
         this.root = _jsArray('', data, this.config.rootSort);
-        console.timeEnd('Render JSONTree array root');
       }
 
       if (isObject(data)) {
-        console.time('Render JSONTree object root');
         this.root = _jsObject('', data, this.config.rootSort);
-        console.timeEnd('Render JSONTree object root');
       }
 
       if (!this.listenersAttached) {

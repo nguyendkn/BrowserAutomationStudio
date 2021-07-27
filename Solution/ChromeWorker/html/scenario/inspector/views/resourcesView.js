30ec186a68b145b828a5fd0d8af4a250ac91e2f0
CheshireCaat <likangt2012@gmail.com>
2021-07-27 03:41:52 +0300

    Update models

--- Solution/ChromeWorker/html/scenario/inspector/views/resourcesView.js
+++ Solution/ChromeWorker/html/scenario/inspector/views/resourcesView.js
@@ -19,20 +19,20 @@
       const source = this.get('source');
       this.set('source', data);
 
-      if (this.get('supportHighlight')) {
-        const diff = jsonpatch.compare(source, data), time = Date.now();
+      const diff = jsonpatch.compare(source, data), time = Date.now();
 
-        diff.forEach(({ path, value, op }) => {
-          if (!_.has(metadata, path)) {
-            metadata[path] = { usage: 6, value, op, addedAt: time, changedAt: time };
-          } else {
-            if (op === 'remove') {
-              return (delete metadata[path]);
-            }
-            metadata[path].changedAt = time;
+      diff.forEach(({ path, value, op }) => {
+        if (!_.has(metadata, path)) {
+          metadata[path] = { usage: 6, value, op, addedAt: time, changedAt: time };
+        } else {
+          if (op === 'remove') {
+            return (delete metadata[path]);
           }
-        });
+          metadata[path].changedAt = time;
+        }
+      });
 
+      if (this.get('supportHighlight')) {
         if (this.get('highlight')) _.each(metadata, (item, path) => {
           item.usage = diff.some(v => v.path === path) ? 1 : (item.usage + 1);
           this.trigger('highlight', { ...item, path });

e9a42733e07918682854f93358c4a9c6c5867754
CheshireCaat <likangt2012@gmail.com>
2021-07-30 22:50:07 +0300

    Add base view class for variables and resources

--- Solution/ChromeWorker/html/scenario/inspector/views/variablesView.js
+++ Solution/ChromeWorker/html/scenario/inspector/views/variablesView.js
@@ -1,16 +1,17 @@
 (function (global, $, _) {
-  const { Inspector } = global.Scenario;
+  const { Inspector, JST } = global.Scenario;
+  const { ScriptDataModel, ScriptDataView } = Inspector;
 
-  const Model = Inspector.ScriptDataModel.extend({
+  const Model = ScriptDataModel.extend({
     defaults: function () {
-      return _.extend({}, Inspector.ScriptDataModel.prototype.defaults, {
+      return _.extend({}, ScriptDataModel.prototype.defaults, {
         supportHighlight: true,
       })
     },
   });
 
-  const View = Backbone.View.extend({
-    template: global.Scenario.JST['inspector/variables'],
+  const View = ScriptDataView.extend({
+    template: JST['inspector/variables'],
 
     initialize() {
       const model = new Model();
@@ -64,89 +65,6 @@
       }
 
       return this;
-    },
-
-    filterTree() {
-      const query = this.$('.inspector-filter-input').val().toLowerCase();
-
-      this.$('.jst-root > ul > li').each((__, el) => {
-        const $el = $(el);
-
-        if (query.length) {
-          const $label = $el.children('.jst-property');
-          const text = $label.text().toLowerCase();
-          return $el.toggle(text.includes(query));
-        } else {
-          const { dataset } = $el.children('[data-path]')[0];
-          const types = this.model.get('visibleTypes');
-
-          if (_.has(types, dataset.type)) {
-            return $el.toggle(types[dataset.type]);
-          }
-        }
-
-        $el.show();
-      });
-
-      return this;
-    },
-
-    sortTree(type) {
-      const metadata = this.model.get('metadata');
-
-      tinysort(this.el.querySelectorAll('.jst-root > ul > li'), {
-        sortFunction: (a, b) => {
-          const [path1, path2] = [a, b].map(({ elm }) => {
-            const node = elm.querySelector(':scope > [data-path]');
-            return node.dataset.path;
-          });
-
-          if (type !== 'alphabetically') {
-            const meta1 = metadata[path1], meta2 = metadata[path2];
-
-            if (type === 'byAddedTime') {
-              return meta2.addedAt - meta1.addedAt;
-            } else if (type === 'byChangedTime') {
-              return meta2.changedAt - meta1.changedAt;
-            } else {
-
-            }
-          }
-          return Scenario.utils.sortByLocals(path1.split('/')[1], path2.split('/')[1]);
-        },
-      });
-
-      return this;
-    },
-
-    loadState(state = this.model.get('state')) {
-      [state.objects, state.arrays].forEach((data) => {
-        if (Array.isArray(data)) {
-          data.forEach(({ path, folded }) => {
-            const $el = this.$el.find(`[data-path="${path}"]`);
-            if (folded && !$el.hasClass('jst-collapsed')) {
-              $el.prev('.jst-collapse').click();
-            }
-          });
-        }
-      });
-
-      this.model.set('state', state);
-    },
-
-    saveState() {
-      this.model.set('state', {
-        objects: _.map(this.$el.find('[data-type="object"]'), el => ({
-          folded: el.classList.contains('jst-collapsed'),
-          path: el.dataset.path,
-        })),
-        arrays: _.map(this.$el.find('[data-type="array"]'), el => ({
-          folded: el.classList.contains('jst-collapsed'),
-          path: el.dataset.path,
-        })),
-      });
-
-      return this.model.get('state');
     },
 
     events: {

4e3a622f79c12b7af481b53aeee4d7449065d6a1
CheshireCaat <likangt2012@gmail.com>
2021-08-04 14:04:26 +0300

    Safe variable update

--- Solution/ChromeWorker/html/scenario/inspector/internal/utils.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/utils.js
@@ -1,4 +1,4 @@
-(({ Scenario }, $, _) =>  {
+(({ Scenario }, $, _) => {
   Scenario.utils = {
     updateVariable(variable, pointer, type) {
       const { root, path, isLocal, isGlobal } = pointer.slice(1).split('/').reduce((data, key, idx) => {
@@ -12,26 +12,28 @@
         return data;
       }, { path: '', root: '', isLocal: true, isGlobal: false });
 
-      if (type === 'date') {
-        variable = `_parse_date('${variable}', 'auto')`;
-      } else if (type === 'custom') {
-        variable = JSON.stringify(eval(`(${variable})`));
-      } else if (type === 'string') {
-        variable = JSON.stringify(variable);
-      }
-
-      VariablesNeedRefresh = true;
-      BrowserAutomationStudio_Execute(`try {
-        if (${isGlobal}) {
-          var obj = JSON.parse(P('basglobal', '${root}') || '{}');
-          obj${path} = ${variable};
-          PSet('basglobal', '${root}', JSON.stringify(obj));
-          delete obj;
-        } else {
-          VAR_${root}${path} = ${variable};
+      _.attempt(() => {
+        if (type === 'date') {
+          variable = `_parse_date('${variable}', 'auto')`;
+        } else if (type === 'custom') {
+          variable = JSON.stringify(eval(`(${variable})`));
+        } else if (type === 'string') {
+          variable = JSON.stringify(variable);
         }
-        section_start('test', -3)!
-      } catch (e) {}`, false);
+
+        VariablesNeedRefresh = true;
+        BrowserAutomationStudio_Execute(`try {
+          if (${isGlobal}) {
+            var obj = JSON.parse(P('basglobal', '${root}') || '{}');
+            obj${path} = ${variable};
+            PSet('basglobal', '${root}', JSON.stringify(obj));
+            delete obj;
+          } else {
+            VAR_${root}${path} = ${variable};
+          }
+          section_start('test', -3)!
+        } catch (e) {}`, false);
+      });
     },
 
     sortByGlobals(a, b) {

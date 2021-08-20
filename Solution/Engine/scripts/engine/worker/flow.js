eeb1f6d564d35162a7c7a774ab2ed2dfce25b61b
CheshireCaat <likangt2012@gmail.com>
2021-08-20 20:03:00 +0300

    Don't send `_Label` cycle property

--- Solution/Engine/scripts/engine/worker/flow.js
+++ Solution/Engine/scripts/engine/worker/flow.js
@@ -50,7 +50,6 @@ function debug_variables(list, callback) {
 
     var callstack = CYCLES.Data.map(function (item) {
         return {
-            label: item._Label,
             action: item._Action,
             iterator: item._Iterator,
             arguments: item._Arguments,

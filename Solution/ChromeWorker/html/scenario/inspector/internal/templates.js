a219b47eec5e31a75b297c446e4c0ef5b2167142
CheshireCaat <likangt2012@gmail.com>
2021-08-25 15:10:57 +0300

    Preserve callstack items state after re-rendering

--- Solution/ChromeWorker/html/scenario/inspector/internal/templates.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/templates.js
@@ -132,7 +132,7 @@ _.extend(App.JST, {
     <% if (stack.length) { %>
       <ul class="callstack-data" style="display: flex; flex-flow: column; padding: 0;">
         <% _.each(stack, item => { %>
-          <% const paramsId = (item.type === 'function' && !_.isEmpty(item.arguments)) ? _.uniqueId('params') : '', expanded = _.has(state, item.id) ? !state[item.id] : false %>
+          <% const paramsId = (item.type === 'function' && !_.isEmpty(item.arguments)) ? _.uniqueId('params') : '', expanded = _.has(state, item.id) && !state[item.id] %>
           <li class="callstack-item" data-id="<%= item.id %>" data-type="<%= item.type %>" style="<%= paramsId ? 'border-color: #c4c4c4' : '' %>">
             <div style="display: flex; justify-content: space-between;">
               <div style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">

b92968334292caf867bb82d98842e394c6088663
CheshireCaat <likangt2012@gmail.com>
2021-09-03 01:29:57 +0300

    Update translate.js

--- Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
@@ -1,62 +1,64 @@
-_.extend(_L, {
-  'Data will be loaded at the next script pause': {
-    ru: 'Данные будут загружены при следующей паузе сценария'
-  },
-  'Show or hide function params': {
-    ru: 'Показать или скрыть параметры функции'
-  },
-  'Change the variable value': {
-    ru: 'Изменить значение переменной'
-  },
-  'By frequency of use': {
-    ru: 'По частоте использования'
-  },
-  'By date modified': {
-    ru: 'По дате изменения'
-  },
-  'By date added': {
-    ru: 'По дате добавления'
-  },
-  'Filter by name': {
-    ru: 'Фильтр по имени'
-  },
-  'Alphabetically': {
-    ru: 'По алфавиту'
-  },
-  'No variables': {
-    ru: 'Нет переменных'
-  },
-  'No resources': {
-    ru: 'Нет ресурсов'
-  },
-  'Call stack': {
-    ru: 'Стек вызовов'
-  },
-  'Variables': {
-    ru: 'Переменные'
-  },
-  'Resources': {
-    ru: 'Ресурсы'
-  },
-  'Functions': {
-    ru: 'Функции'
-  },
-  'Actions': {
-    ru: 'Действия'
-  },
-  'Sorting': {
-    ru: 'Сортировка'
-  },
-  'Filters': {
-    ru: 'Фильтры'
-  },
-  'Accept': {
-    ru: 'Принять'
-  },
-  'String': {
-    ru: 'Строка'
-  },
-  'Number': {
-    ru: 'Число'
-  }
-});
+(({ App, _ }) => {
+  _.extend(_L, {
+    'Data will be loaded at the next script pause': {
+      ru: 'Данные будут загружены при следующей паузе сценария'
+    },
+    'Show or hide function params': {
+      ru: 'Показать или скрыть параметры функции'
+    },
+    'Change the variable value': {
+      ru: 'Изменить значение переменной'
+    },
+    'By frequency of use': {
+      ru: 'По частоте использования'
+    },
+    'By date modified': {
+      ru: 'По дате изменения'
+    },
+    'By date added': {
+      ru: 'По дате добавления'
+    },
+    'Filter by name': {
+      ru: 'Фильтр по имени'
+    },
+    'Alphabetically': {
+      ru: 'По алфавиту'
+    },
+    'No variables': {
+      ru: 'Нет переменных'
+    },
+    'No resources': {
+      ru: 'Нет ресурсов'
+    },
+    'Call stack': {
+      ru: 'Стек вызовов'
+    },
+    'Variables': {
+      ru: 'Переменные'
+    },
+    'Resources': {
+      ru: 'Ресурсы'
+    },
+    'Functions': {
+      ru: 'Функции'
+    },
+    'Actions': {
+      ru: 'Действия'
+    },
+    'Sorting': {
+      ru: 'Сортировка'
+    },
+    'Filters': {
+      ru: 'Фильтры'
+    },
+    'Accept': {
+      ru: 'Принять'
+    },
+    'String': {
+      ru: 'Строка'
+    },
+    'Number': {
+      ru: 'Число'
+    }
+  });
+})(window);

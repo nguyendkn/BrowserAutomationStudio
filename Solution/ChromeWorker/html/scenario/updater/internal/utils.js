581b3beff894a413e0206ba8d6e6597aabb49534
CheshireCaat <likangt2012@gmail.com>
2021-09-02 04:05:52 +0300

    Move underscore mixins to the global `utils` file

--- Solution/ChromeWorker/html/scenario/updater/internal/utils.js
+++ Solution/ChromeWorker/html/scenario/updater/internal/utils.js
@@ -28,48 +28,4 @@
       }
     });
   };
-
-  _.mixin({
-    attempt: function (func, ...args) {
-      try {
-        return func(...args);
-      } catch (e) {
-        return _.isError(e) ? e : new Error(e);
-      }
-    },
-
-    truncate: function (str, limit) {
-      return str.length > limit ? (str.slice(0, limit - 3) + '...') : str;
-    },
-
-    concat: function (arr, ...args) {
-      return arr.concat(...args);
-    },
-
-    slice: function (arr, ...args) {
-      return arr.slice(...args);
-    },
-
-    upperFirst: function (str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    },
-
-    lowerFirst: function (str) {
-      return str.charAt(0).toLowerCase() + str.slice(1);
-    },
-
-    isString: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object String]';
-    },
-
-    isError: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Error]';
-    },
-
-    sleep: function (time) {
-      return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-      });
-    }
-  });
 })(window);
--- Solution/ChromeWorker/html/scenario/utility/utils.js
+++ Solution/ChromeWorker/html/scenario/utility/utils.js
@@ -1,7 +1,7 @@
 (({ App, Backbone, $, _ }) => {
   App.utils = {
     getTaskInfo(task) {
-      if (!(task instanceof Backbone.Model)) return {}; 
+      if (!(task instanceof Backbone.Model)) return {};
       const dat = _.attempt(() => task.dat());
       const isDamaged = _.isError(dat);
       const isEmpty = _.isNull(dat);
@@ -21,5 +21,49 @@
     container: false,
     header: false,
     width: false,
+  });
+
+  _.mixin({
+    attempt: function (func, ...args) {
+      try {
+        return func(...args);
+      } catch (e) {
+        return _.isError(e) ? e : new Error(e);
+      }
+    },
+
+    truncate: function (str, limit) {
+      return str.length > limit ? (str.slice(0, limit - 3) + '...') : str;
+    },
+
+    concat: function (arr, ...args) {
+      return arr.concat(...args);
+    },
+
+    slice: function (arr, ...args) {
+      return arr.slice(...args);
+    },
+
+    upperFirst: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
+    lowerFirst: function (str) {
+      return str.charAt(0).toLowerCase() + str.slice(1);
+    },
+
+    isString: function (obj) {
+      return Object.prototype.toString.call(obj) === '[object String]';
+    },
+
+    isError: function (obj) {
+      return Object.prototype.toString.call(obj) === '[object Error]';
+    },
+
+    sleep: function (time) {
+      return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+      });
+    }
   });
 })(window);

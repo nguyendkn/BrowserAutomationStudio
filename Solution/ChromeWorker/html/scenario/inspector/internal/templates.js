6918ce570347a660b124b477a42c4d718e901bf7
CheshireCaat <likangt2012@gmail.com>
2021-09-06 18:47:29 +0300

    Apply filters for callstack items after rendering

--- Solution/ChromeWorker/html/scenario/inspector/internal/templates.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/templates.js
@@ -134,7 +134,7 @@
         <ul class="callstack-data">
           <% _.each(stack, ({ id, type, name, ...item }) => { %>
             <% const paramsId = (type === 'function' && !_.isEmpty(item.arguments)) ? _.uniqueId('params') : '', expanded = _.has(state, id) && !state[id] %>
-            <li class="callstack-item" data-id="<%= id %>" data-type="<%= type %>" style="<%= paramsId ? 'border-color: #c4c4c4' : '' %>">
+            <li class="callstack-item" data-id="<%= id %>" data-type="<%= type %>" style="<%= paramsId ? 'border-color: #c4c4c4;' : '' %><%= filters[type + 's'] ? '' : 'display: none;' %>">
               <div>
                 <span class="callstack-item-name"><%= name + (type === 'action' ? ':' : '') %></span>
                 <% if (type === 'action') { %>

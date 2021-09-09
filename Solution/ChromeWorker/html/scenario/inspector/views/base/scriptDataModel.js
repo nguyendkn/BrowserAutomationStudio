e258b71162ca24da07c1a07f132d0fc3b1f3f393
CheshireCaat <likangt2012@gmail.com>
2021-09-09 16:12:55 +0300

    Update scriptDataModel.js

--- Solution/ChromeWorker/html/scenario/inspector/views/base/scriptDataModel.js
+++ Solution/ChromeWorker/html/scenario/inspector/views/base/scriptDataModel.js
@@ -2,42 +2,6 @@
   const { Inspector } = App;
 
   Inspector.ScriptDataModel = Backbone.Model.extend({
-    update: function (source) {
-      const highlight = this.get('highlight');
-      const metadata = this.get('metadata');
-      let updates = this.get('updates');
-      let history = this.get('history');
-
-      const diff = jsonpatch.compare(this.get('source'), source); diff.forEach(item => {
-        const { path, op } = item, time = performance.now();
-
-        if (!_.has(metadata, path)) {
-          metadata[path] = { count: 5, usages: 1, addedAt: time, modifiedAt: time };
-        } else {
-          if (op === 'remove') return (delete metadata[path]);
-          metadata[path].modifiedAt = time;
-          metadata[path].usages += 1;
-          metadata[path].count += 0;
-        }
-
-        history = history.concat(path).slice(-100);
-      });
-
-      updates += diff.length !== 0;
-      this.set('updates', updates);
-      this.set('history', history);
-      this.set('source', source);
-      {
-        _.each(metadata, (item, path) => {
-          if (highlight) {
-            item.count = diff.some(v => v.path === path) ? 0 : Math.min(item.count + 1, 5);
-          }
-          this.trigger('highlight', { count: item.count, path });
-        });
-        this.set('highlight', false);
-      }
-    },
-
     defaults: () => ({
       sorting: 'alphabetically',
       highlight: false,
@@ -57,6 +21,42 @@
         date: true,
         null: true
       }
-    })
+    }),
+
+    update: function (source) {
+      const highlight = this.get('highlight');
+      const metadata = this.get('metadata');
+      let updates = this.get('updates');
+      let history = this.get('history');
+
+      const diff = jsonpatch.compare(this.get('source'), source); diff.forEach(item => {
+        const { path, op } = item, now = performance.now();
+
+        if (_.has(metadata, path)) {
+          if (op === 'remove') return (delete metadata[path]);
+          metadata[path].modifiedAt = now;
+          metadata[path].usages += 1;
+          metadata[path].count += 0;
+        } else {
+          metadata[path] = { modifiedAt: now, addedAt: now, usages: 1, count: 5 };
+        }
+
+        history = history.concat(path).slice(-100);
+      });
+
+      updates += diff.length !== 0;
+      this.set('updates', updates);
+      this.set('history', history);
+      this.set('source', source);
+      {
+        _.each(metadata, (item, path) => {
+          if (highlight) {
+            item.count = diff.some(v => v.path === path) ? 0 : Math.min(item.count + 1, 5);
+          }
+          this.trigger('highlight', { count: item.count, path });
+        });
+        this.set('highlight', false);
+      }
+    }
   });
 })(window);

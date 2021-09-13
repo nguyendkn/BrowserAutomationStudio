6019b03caf5b9ccaae4573c79f80a015815ace79
CheshireCaat <likangt2012@gmail.com>
2021-09-13 15:33:43 +0300

    Allow to open modal for resources (except modifying)

--- Solution/ChromeWorker/html/scenario/inspector/views/resourcesView.js
+++ Solution/ChromeWorker/html/scenario/inspector/views/resourcesView.js
@@ -10,8 +10,29 @@
       ScriptDataView.prototype.initialize.call(this, { allowHighlight: false });
     },
 
+    openModal: function (e) {
+      e.stopPropagation();
+      const { path, type } = e.target.closest('li').dataset;
+
+      const modal = new Inspector.Modal({
+        callback({ isChanged, value, cancel, type }) {
+          if (cancel || !isChanged) return;
+          // updateVariable(value, path, type);
+        },
+        value: this.viewer.model.getValue(path),
+        type,
+        path
+      });
+
+      modal.render();
+    },
+
     events: {
-      ...ScriptDataView.prototype.events
+      ...ScriptDataView.prototype.events,
+
+      'dblclick .jst-root > li > ul > li > .jst-node': 'openModal',
+
+      'dblclick .jst-root > li > ul > li > .jst-list': 'openModal',
     }
   });
 })(window);

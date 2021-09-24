44287c2b6429095ca8cb437b84475bbac817016d
CheshireCaat <likangt2012@gmail.com>
2021-09-24 13:29:15 +0300

    Update translate.js

--- Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
@@ -1,76 +1,28 @@
 (({ App, _ }) => {
   _.extend(_L, {
-    'Data will be loaded at the next script pause': {
-      ru: 'Данные будут загружены при следующей паузе сценария'
-    },
-    'Click to show or hide function params': {
-      ru: 'Кликните, чтобы показать или скрыть параметры функции'
-    },
-    'Click to show or hide the full text': {
-      ru: 'Кликните, чтобы показать или скрыть полный текст'
-    },
-    'Change the variable value': {
-      ru: 'Изменить значение переменной'
-    },
-    'By frequency of use': {
-      ru: 'По частоте использования'
-    },
-    'By date modified': {
-      ru: 'По дате изменения'
-    },
-    'By date created': {
-      ru: 'По дате создания'
-    },
-    'Filter by name': {
-      ru: 'Фильтр по имени'
-    },
-    'Alphabetically': {
-      ru: 'По алфавиту'
-    },
-    'No variables': {
-      ru: 'Нет переменных'
-    },
-    'No resources': {
-      ru: 'Нет ресурсов'
-    },
-    'Call stack': {
-      ru: 'Стек вызовов'
-    },
-    'Variables': {
-      ru: 'Переменные'
-    },
-    'Resources': {
-      ru: 'Ресурсы'
-    },
-    'Functions': {
-      ru: 'Функции'
-    },
-    'Actions': {
-      ru: 'Действия'
-    },
-    'Sorting': {
-      ru: 'Сортировка'
-    },
-    'Filters': {
-      ru: 'Фильтры'
-    },
-    'Groups': {
-      ru: 'Группы'
-    },
-    'Rename': {
-      ru: 'Переименовать'
-    },
-    'Update': {
-      ru: 'Обновить'
-    },
-    'String': {
-      ru: 'Строка'
-    },
-    'Array': {
-      ru: 'Массив'
-    },
-    'Number': {
-      ru: 'Число'
-    }
+    'Data will be loaded at the next script pause': { ru: 'Данные будут загружены при следующей паузе сценария' },
+    'Click to show or hide function params': { ru: 'Кликните, чтобы показать или скрыть параметры функции' },
+    'Click to show or hide the full text': { ru: 'Кликните, чтобы показать или скрыть полный текст' },
+    'Change the variable value': { ru: 'Изменить значение переменной' },
+    'By frequency of use': { ru: 'По частоте использования' },
+    'By date modified': { ru: 'По дате изменения' },
+    'By date created': { ru: 'По дате создания' },
+    'Filter by name': { ru: 'Фильтр по имени' },
+    'Alphabetically': { ru: 'По алфавиту' },
+    'No variables': { ru: 'Нет переменных' },
+    'No resources': { ru: 'Нет ресурсов' },
+    'Call stack': { ru: 'Стек вызовов' },
+    'Variables': { ru: 'Переменные' },
+    'Resources': { ru: 'Ресурсы' },
+    'Functions': { ru: 'Функции' },
+    'Actions': { ru: 'Действия' },
+    'Sorting': { ru: 'Сортировка' },
+    'Filters': { ru: 'Фильтры' },
+    'Groups': { ru: 'Группы' },
+    'Rename': { ru: 'Переименовать' },
+    'Update': { ru: 'Обновить' },
+    'String': { ru: 'Строка' },
+    'Array': { ru: 'Массив' },
+    'Number': { ru: 'Число' },
   });
 })(window);

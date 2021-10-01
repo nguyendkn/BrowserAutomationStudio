9bf3ad36457da28b135755e99aa146c9316836c0
CheshireCaat <likangt2012@gmail.com>
2021-10-02 00:07:55 +0300

    Add simple tabs

--- Solution/ChromeWorker/html/inspector/js/components/App.js
+++ Solution/ChromeWorker/html/inspector/js/components/App.js
@@ -1,12 +1,37 @@
 window.App = {
   template: /*html*/`
-    <div>{{title}}</div>
+    <div>
+      <nav>
+        <li>
+          <a v-on:click.prevent="setActive('variables')" :class="{ active: isActive('variables') }" href="#variables">Variables</a>
+        </li>
+        <li>
+          <a v-on:click.preventt="setActive('resources')" :class="{ active: isActive('resources') }" href="#resources">Resources</a>
+        </li>
+        <li>
+          <a v-on:click.prevent="setActive('callstack')" :class="{ active: isActive('callstack') }" href="#callstack">Callstack</a>
+        </li>
+      </nav>
+      <div>
+        <div v-show="isActive('variables')" id="variables">Variables</div>
+        <div v-show="isActive('resources')" id="resources">Resources</div>
+        <div v-show="isActive('callstack')" id="callstack">Callstack</div>
+      </div>
+    </div>
   `,
   name: 'App',
   components: {},
   data() {
     return {
-      title: 'Inspector'
+      activeTab: 'variables'
     };
+  },
+  methods: {
+    isActive(tab) {
+      return this.activeTab === tab;
+    },
+    setActive(tab) {
+      this.activeTab = tab;
+    }
   }
 };

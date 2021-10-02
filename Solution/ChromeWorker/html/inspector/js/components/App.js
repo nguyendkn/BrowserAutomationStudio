c6c70d60ea08ae7e021923d56bac93ebf6f00335
CheshireCaat <likangt2012@gmail.com>
2021-10-02 19:41:03 +0300

    Update app component template

--- Solution/ChromeWorker/html/inspector/js/components/App.js
+++ Solution/ChromeWorker/html/inspector/js/components/App.js
@@ -1,27 +1,40 @@
 window.App = {
   template: /*html*/`
     <div>
-      <nav>
-        <li :class="{ active: isActive('variables') }">
-          <a v-on:click.prevent="setActive('variables')" href="#variables">
-            {{ $t('nav.variables') }}
-          </a>
-        </li>
-        <li :class="{ active: isActive('resources') }">
-          <a v-on:click.prevent="setActive('resources')" href="#resources">
-            {{ $t('nav.resources') }}
-          </a>
-        </li>
-        <li :class="{ active: isActive('callstack') }">
-          <a v-on:click.prevent="setActive('callstack')" href="#callstack">
-            {{ $t('nav.callstack') }}
-          </a>
-        </li>
-      </nav>
       <div>
-        <div v-show="isActive('variables')" id="variables">Variables</div>
-        <div v-show="isActive('resources')" id="resources">Resources</div>
-        <div v-show="isActive('callstack')" id="callstack">Call stack</div>
+        <ul>
+          <li :class="{ active: isActive('variables') }">
+            <a v-on:click.prevent="setActive('variables')" href="#variables">
+              {{ $t('nav.variables') }}
+            </a>
+          </li>
+          <li :class="{ active: isActive('resources') }">
+            <a v-on:click.prevent="setActive('resources')" href="#resources">
+              {{ $t('nav.resources') }}
+            </a>
+          </li>
+          <li :class="{ active: isActive('callstack') }">
+            <a v-on:click.prevent="setActive('callstack')" href="#callstack">
+              {{ $t('nav.callstack') }}
+            </a>
+          </li>
+        </ul>
+        <button v-on:click="hide()" type="button" style="min-width: 28px; border: none; background: #fafafa;">
+          <svg viewBox="0 0 12 12" height="12" width="12" fill="#000" style="vertical-align: middle;">
+            <path d="M12 1.0501l-1.05-1.05L6 4.9501 1.05.0001 0 1.0501l4.95 4.95-4.95 4.95 1.05 1.05L6 7.0501l4.95 4.95 1.05-1.05-4.95-4.95 4.95-4.95z" />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <div v-show="isActive('variables')" id="variables">
+          Variables content
+        </div>
+        <div v-show="isActive('resources')" id="resources">
+          Resources content
+        </div>
+        <div v-show="isActive('callstack')" id="callstack">
+          Call stack content
+        </div>
       </div>
     </div>
   `,

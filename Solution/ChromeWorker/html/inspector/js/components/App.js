18d4b7808b0efa5f049eff3cea1bf57edac29af2
CheshireCaat <likangt2012@gmail.com>
2021-10-02 19:54:55 +0300

    Update vue component code using official style guide

--- Solution/ChromeWorker/html/inspector/js/components/App.js
+++ Solution/ChromeWorker/html/inspector/js/components/App.js
@@ -1,25 +1,59 @@
 window.App = {
+  name: 'App',
+
+  data: () => ({
+    activeTab: 'variables'
+  }),
+
+  destroyed() {
+    window.removeEventListener('message', this.handleFrameEvent);
+  },
+
+  mounted() {
+    window.addEventListener('message', this.handleFrameEvent);
+  },
+
+  methods: {
+    isActive(tab) {
+      return this.activeTab === tab;
+    },
+
+    setActive(tab) {
+      this.activeTab = tab;
+    },
+
+    handleFrameEvent({ data }) {
+      console.log(data)
+    },
+
+    hide() {
+      window.parent.postMessage({
+        type: 'hide'
+      }, '*')
+    }
+  },
+
   template: /*html*/`
     <div>
       <div>
         <ul>
           <li :class="{ active: isActive('variables') }">
-            <a v-on:click.prevent="setActive('variables')" href="#variables">
+            <a @click.prevent="setActive('variables')" href="#variables">
               {{ $t('nav.variables') }}
             </a>
           </li>
           <li :class="{ active: isActive('resources') }">
-            <a v-on:click.prevent="setActive('resources')" href="#resources">
+            <a @click.prevent="setActive('resources')" href="#resources">
               {{ $t('nav.resources') }}
             </a>
           </li>
           <li :class="{ active: isActive('callstack') }">
-            <a v-on:click.prevent="setActive('callstack')" href="#callstack">
+            <a @click.prevent="setActive('callstack')" href="#callstack">
               {{ $t('nav.callstack') }}
             </a>
           </li>
         </ul>
-        <button v-on:click="hide()" type="button" style="min-width: 28px; border: none; background: #fafafa;">
+        <button @click="hide()" type="button" style="min-width: 28px; border: none; background: #fafafa;">
           <svg viewBox="0 0 12 12" height="12" width="12" fill="#000" style="vertical-align: middle;">
             <path d="M12 1.0501l-1.05-1.05L6 4.9501 1.05.0001 0 1.0501l4.95 4.95-4.95 4.95 1.05 1.05L6 7.0501l4.95 4.95 1.05-1.05-4.95-4.95 4.95-4.95z" />
           </svg>
@@ -38,30 +72,4 @@ window.App = {
       </div>
     </div>
   `,
-  name: 'App',
-  data: () => ({
-    activeTab: 'variables'
-  }),
-  methods: {
-    isActive(tab) {
-      return this.activeTab === tab;
-    },
-    setActive(tab) {
-      this.activeTab = tab;
-    },
-    handleFrameEvent({ data }) {
-      console.log(data)
-    },
-    hide() {
-      window.parent.postMessage({
-        type: 'hide'
-      }, '*')
-    }
-  },
-  destroyed() {
-    window.removeEventListener('message', this.handleFrameEvent);
-  },
-  mounted() {
-    window.addEventListener('message', this.handleFrameEvent);
-  }
 };
--- Solution/ChromeWorker/html/inspector/js/main.js
+++ Solution/ChromeWorker/html/inspector/js/main.js
@@ -10,8 +10,6 @@ const app = new Vue({
     /* 
       'Data will be loaded at the next script pause': { ru: 'Данные будут загружены при следующей паузе сценария' },
       'Change the variable value': { ru: 'Изменить значение переменной' },
-      'No variables': { ru: 'Нет переменных' },
-      'No resources': { ru: 'Нет ресурсов' },
       'Update': { ru: 'Обновить' },
     */
   })

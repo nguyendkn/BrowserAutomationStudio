b5c7de46d98bcf3fec314e3e2852f5e483bf882b
CheshireCaat <likangt2012@gmail.com>
2021-10-02 15:41:30 +0300

    Update main.js

--- Solution/ChromeWorker/html/inspector/js/main.js
+++ Solution/ChromeWorker/html/inspector/js/main.js
@@ -4,20 +4,16 @@ const app = new Vue({
   render: h => h(App),
 
   i18n: new VueI18n({
-    messages: {
-      /* 
+    messages: { ...window.locales },
+    locale: new URL(location.href).searchParams.get('lang') || 'en',
+    fallbackLocale: 'en'
+    /* 
       'Data will be loaded at the next script pause': { ru: 'Данные будут загружены при следующей паузе сценария' },
       'Change the variable value': { ru: 'Изменить значение переменной' },
       'No variables': { ru: 'Нет переменных' },
       'No resources': { ru: 'Нет ресурсов' },
-      'Rename': { ru: 'Переименовать' },
       'Update': { ru: 'Обновить' },
-      */
-      en: window.locales.en,
-      ru: window.locales.ru,
-    },
-    locale: new URL(location.href).searchParams.get('lang') || 'en',
-    fallbackLocale: 'en'
+    */
   })
 });
 

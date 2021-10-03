ba78cd9eefa5455e3daf9d3634093604f009ac50
CheshireCaat <likangt2012@gmail.com>
2021-10-04 00:58:12 +0300

    Update
    Add icons for tabs to the assets folder.
    Update styles (component).
    Update styles (global).

--- Solution/ChromeWorker/html/inspector/src/App.js
+++ Solution/ChromeWorker/html/inspector/src/App.js
@@ -54,7 +54,7 @@ window.App = {
             </a>
           </li>
         </ul>
-        <button @click="hide()" type="button" style="min-width: 28px; border: none; background: #fafafa;">
+        <button @click="hide()" type="button" style="min-width: 28px; border: none; background: #f6f6f6;">
           <svg viewBox="0 0 12 12" height="12" width="12" fill="#000" style="vertical-align: middle;">
             <path d="M12 1.0501l-1.05-1.05L6 4.9501 1.05.0001 0 1.0501l4.95 4.95-4.95 4.95 1.05 1.05L6 7.0501l4.95 4.95 1.05-1.05-4.95-4.95 4.95-4.95z" />
           </svg>

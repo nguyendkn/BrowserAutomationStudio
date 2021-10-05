4d936a87d1c10595a653348ac9afde9cf333a0d3
CheshireCaat <likangt2012@gmail.com>
2021-10-05 18:35:18 +0300

    Update toolbar and styles
    Move styles for toolbar from old to new version.
    Remove custom styles for callstack toolbar.

--- Solution/ChromeWorker/html/inspector/src/components/Toolbar.js
+++ Solution/ChromeWorker/html/inspector/src/components/Toolbar.js
@@ -21,17 +21,19 @@ window.Toolbar = {
   },
 
   template: html`
-    <div class="inspector-tools-panel">
-      <input v-model="query" type="search" class="inspector-tools-input" :placeholder="$t('toolbar.placeholder') + '...'">
-      <div class="dropdown">
-        <button data-toggle="dropdown" type="button" aria-expanded="false" aria-haspopup="true">
-          <i class="fa fa-filter"></i>
-        </button>
-        <ul class="dropdown-menu dropdown-menu-right inspector-tools-menu">
-          <li v-for="item in sortings" :key="item" @click="selectedSorting = item">
-            <a href="#">{{ $t('toolbar.sorting.' + item) }}</a>
-          </li>
-        </ul>
+    <div class="inspector-tools">
+      <div class="inspector-tools-panel">
+        <input v-model="query" type="search" class="inspector-tools-input" :placeholder="$t('toolbar.placeholder') + '...'">
+        <div class="dropdown">
+          <button data-toggle="dropdown" type="button" aria-expanded="false" aria-haspopup="true">
+            <i class="fa fa-filter"></i>
+          </button>
+          <ul class="dropdown-menu dropdown-menu-right inspector-tools-menu">
+            <li v-for="item in sortings" :key="item" @click="selectedSorting = item">
+              <a href="#">{{ $t('toolbar.sorting.' + item) }}</a>
+            </li>
+          </ul>
+        </div>
       </div>
       <button class="inspector-tools-toggle" type="button">
         <i class="fa fa-chevron-up"></i>

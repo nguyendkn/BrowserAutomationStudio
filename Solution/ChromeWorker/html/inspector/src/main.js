5209ca2921bc0b09d00d5f0b1100317122487d96
CheshireCaat <likangt2012@gmail.com>
2021-10-06 03:36:40 +0300

    Update code and styles

--- Solution/ChromeWorker/html/inspector/src/main.js
+++ Solution/ChromeWorker/html/inspector/src/main.js
@@ -4,7 +4,7 @@ const app = new Vue({
   render: h => h(App),
 
   i18n: new VueI18n({
-    locale: new URL(location.href).searchParams.get('lang') || 'en',
+    locale: new URL(window.location.href).searchParams.get('lang') || 'en',
     messages: { ...locales },
     fallbackLocale: 'en'
   })

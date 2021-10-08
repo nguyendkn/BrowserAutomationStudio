df80edca4338418e5a06469fd7b04b993a128b32
CheshireCaat <likangt2012@gmail.com>
2021-10-08 18:27:31 +0300

    Update main.js

--- Solution/ChromeWorker/html/inspector/src/main.js
+++ Solution/ChromeWorker/html/inspector/src/main.js
@@ -1,11 +1,11 @@
 Vue.config.productionTip = false;
 
 const app = new Vue({
-  render: h => h(App),
-
   i18n: new VueI18n({
     locale: new URL(window.location.href).searchParams.get('lang') || 'en',
     messages: { ...locales },
     fallbackLocale: 'en'
-  })
+  }),
+
+  render: h => h(App)
 }).$mount('#app');

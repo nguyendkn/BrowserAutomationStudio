91b5f8f9501888a05d60f59847267ea4717efa09
CheshireCaat <likangt2012@gmail.com>
2021-10-11 12:12:08 +0300

    Update Toolbar.js

--- Solution/ChromeWorker/html/scenario/inspector/new/src/components/Toolbar.js
+++ Solution/ChromeWorker/html/scenario/inspector/new/src/components/Toolbar.js
@@ -37,6 +37,10 @@ window.Toolbar = {
 
     toggleDropdown() {
       this.dropdown = !this.dropdown;
+    },
+
+    toggleVisible() {
+      this.visible = !this.visible;
     }
   },
 
@@ -67,7 +71,7 @@ window.Toolbar = {
           </div>
         </div>
       </CollapseTransition>
-      <button class="app-toolbar-toggle" type="button" @click="visible = !visible">
+      <button @click="toggleVisible" class="app-toolbar-toggle" type="button">
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" :style="{ transform: visible ? '' : 'rotate(180deg)' }">
           <path fill="#606060" d="M3.51482 9.79281L7.75743 5.55014L12.0001 9.79284L11.2931 10.5L7.75754 6.96435L4.22192 10.4999L3.51482 9.79281Z" />
         </svg>

7a38adfd0b0a72dafd557cbc38797d0915287941
CheshireCaat <likangt2012@gmail.com>
2021-10-13 11:55:04 +0300

    Restore translation file for inspector

---
+++ Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
@@ -0,0 +1,7 @@
+(({ App, _ }) => {
+  _.extend(_L, {
+    'Copy to clipboard': { ru: 'Копировать в буфер обмена' },
+    'Copy the variable name': { ru: 'Копировать имя переменной' },
+    'Search for variable in project': { ru: 'Поиск переменной в проекте' }
+  });
+})(window);

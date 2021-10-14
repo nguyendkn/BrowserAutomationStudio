755e52c50a13e487263b5313fff3f32d172dcadf
CheshireCaat <likangt2012@gmail.com>
2021-10-14 08:23:00 +0300

    Update translate.js

--- Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
+++ Solution/ChromeWorker/html/scenario/inspector/internal/translate.js
@@ -1,5 +1,6 @@
 (({ App, _ }) => {
   _.extend(_L, {
+    'Save changes': { ru: 'Сохранить изменения' },
     'Copy to clipboard': { ru: 'Копировать в буфер обмена' },
     'Copy the variable name': { ru: 'Копировать имя переменной' },
     'Search for variable in project': { ru: 'Поиск переменной в проекте' }

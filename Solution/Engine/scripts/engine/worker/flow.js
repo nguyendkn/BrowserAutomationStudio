73df83d0ae79f5fd3cbe94b7d24ef166b8181cb4
CheshireCaat <likangt2012@gmail.com>
2021-10-24 19:48:40 +0300

    Refactoring `debug_variables` method

--- Solution/Engine/scripts/engine/worker/flow.js
+++ Solution/Engine/scripts/engine/worker/flow.js
@@ -34,46 +34,40 @@ function success(text)
     ScriptWorker.Success(text);
 }
 
-function debug_variables(list, callback) {
-    var variables = list.reduce(function (acc, key) {
-        if (key.indexOf('GLOBAL:') === 0) {
-            acc[key] = JSON.parse(P('basglobal', key.slice(7)) || '"_UNDEFINED_"');
-        } else {
-            acc[key.slice(4)] = truncate_variable(GLOBAL[key], 100);
-        }
-        return acc;
-    }, {});
-
-    var callstack = CYCLES.Data.map(function (item) {
-        var info = item._Info;
-
-        return {
-            type: info.type,
-            name: info.name,
-            id: info.id,
-            options: {
-                expression: info.expression || '',
-                arguments: item._Arguments || {},
-                iterator: item._iterator || 0
+var debug_variables = (function () {
+    return function (list, callback) {
+        var variables = list.reduce(function (acc, key) {
+            if (key.indexOf('GLOBAL:') === 0) {
+                acc[key] = JSON.parse(P('basglobal', key.slice(7)) || '"_UNDEFINED_"');
+            } else {
+                acc[key.slice(4)] = truncate_variable(GLOBAL[key], 100);
             }
-        }
-    }).filter(function (item) { return item.name }).reverse().concat({
-        type: 'function',
-        name: 'Main',
-        id: 0,
-        options: {
-            expression: '',
-            arguments: {},
-            iterator: 0
-        }
-    });
+            return acc;
+        }, {});
 
-    Browser.DebugVariablesResult(JSON.stringify({
-        resources: JSON.parse(ScriptWorker.PickResources()),
-        variables: variables,
-        callstack: callstack,
-    }), _get_function_body(callback));
-}
+        var callstack = CYCLES.Data.map(function (item) { return mapCycle(item, item._Info); })
+            .filter(function (item) { return item.name }).reverse().concat(mapCycle({}, {
+                type: 'function',
+                name: 'Main',
+                id: 0
+            }));
+
+        Browser.DebugVariablesResult(JSON.stringify({
+            resources: JSON.parse(ScriptWorker.PickResources()),
+            variables: variables,
+            callstack: callstack,
+        }), _get_function_body(callback));
+    };
+
+    function mapCycle(item, info) {
+        var options = {
+            expression: info.expression || '',
+            arguments: item._Arguments || {},
+            iterator: item._Iterator || 0
+        };
+        return { type: info.type, name: info.name, id: info.id, options: options };
+    }
+})();
 
 function _read_variables(list)
 {

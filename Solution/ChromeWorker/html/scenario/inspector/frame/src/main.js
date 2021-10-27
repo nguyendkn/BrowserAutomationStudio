535f82ea89dcea2c1709b854d41d2773388da140
CheshireCaat <likangt2012@gmail.com>
2021-10-27 04:11:07 +0300

    Simplify code for pluralization

--- Solution/ChromeWorker/html/scenario/inspector/frame/src/main.js
+++ Solution/ChromeWorker/html/scenario/inspector/frame/src/main.js
@@ -9,8 +9,7 @@ new Vue({
         if (choice === 0) return 0;
 
         const teen = choice > 10 && choice < 20;
-        const endsWithOne = choice % 10 === 1;
-        if (!teen && endsWithOne) return 1;
+        if (!teen && choice % 10 === 1) return 1;
 
         return choicesLength < 4 || (!teen && choice % 10 >= 2 && choice % 10 <= 4) ? 2 : 3;
       }

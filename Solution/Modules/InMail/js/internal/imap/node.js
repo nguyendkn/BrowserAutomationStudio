fd787aa52c6527c810a2aed7e56bcc72d1f655ba
GhostZ <sasha9817@mail.ru>
2021-11-01 17:49:09 +0400

    Delete old files

--- Solution/Modules/InMail/js/internal/imap/node.js
+++
@@ -1,4 +0,0 @@
-_InMail.imap = _InMail.assignApi(function(autoConfig, host, port, encrypt, username, password, folder){
-	const api = this;
-	_InMail.baseApi.call(this, false, "imap", autoConfig, host, port, encrypt, username, password, folder);
-});

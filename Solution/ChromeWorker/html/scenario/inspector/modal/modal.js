74cb734ef8bd6413f1770741f3b9ee705a1ca598
CheshireCaat <likangt2012@gmail.com>
2021-11-23 18:18:31 +0300

    Update modal template

--- Solution/ChromeWorker/html/scenario/inspector/modal/modal.js
+++ Solution/ChromeWorker/html/scenario/inspector/modal/modal.js
@@ -139,7 +139,7 @@
           <div class="inspector-modal-header">
             <h4><%= $t('inspector.header.' + mode, { name }) %></h4>
             <h6><%= $t('inspector.subheader.' + mode) %></h6>
-            <button data-dismiss="modal">
+            <button type="button" data-dismiss="modal" aria-label="Close">
               <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M18 7.05L16.95 6L12 10.95L7.05 6L6 7.05L10.95 12L6 16.95L7.05 18L12 13.05L16.95 18L18 16.95L13.05 12L18 7.05Z" fill="#606060" />
               </svg>

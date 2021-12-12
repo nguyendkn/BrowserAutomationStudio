9dfca9ed654fbf6aa3bde82fce52144e29924476
CheshireCaat <likangt2012@gmail.com>
2021-12-12 19:30:49 +0300

    Update main.js

--- Solution/ChromeWorker/html/scenario/inspector/frame/src/main.js
+++ Solution/ChromeWorker/html/scenario/inspector/frame/src/main.js
@@ -3,8 +3,6 @@
 Vue.use(window['v-click-outside']);
 
 const i18n = new VueI18n({
-  locale: new URL(window.location.href).searchParams.get('lang') || 'en',
-  messages: { ...window.locales },
   pluralizationRules: {
     ru(choice, choicesLength) {
       if (choice === 0) return 0;
@@ -15,6 +13,8 @@ const i18n = new VueI18n({
       return choicesLength < 4 || (!teen && choice % 10 >= 2 && choice % 10 <= 4) ? 2 : 3;
     },
   },
+  locale: new URL(window.location.href).searchParams.get('lang') || 'en',
+  messages: { ...window.locales },
 });
 
 const store = new Vuex.Store({

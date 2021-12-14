285f061e1d38e721fadbca2f8960c46925c16d0a
CheshireCaat <likangt2012@gmail.com>
2021-12-14 22:20:22 +0300

    Added the ability to change the color of the group

--- Solution/ChromeWorker/html/scenario/inspector/frame/src/components/groups/GroupsItem.js
+++ Solution/ChromeWorker/html/scenario/inspector/frame/src/components/groups/GroupsItem.js
@@ -72,13 +72,20 @@ window.GroupsItem = {
   },
 
   template: html`
-    <li class="group-item" :style="{ '--group-color': color }">
+    <li class="group-item" :style="{ '--group-color': newColor }">
       <div class="group-item-header">
         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path d="M6.00024 3.5v-2H.00024414v12H16.0002v-10H6.00024Z" fill="#606060" stroke="#606060" />
         </svg>
-        <input ref="input" v-model="newName" :disabled="!editMode" maxlength="30" spellcheck="false" type="text" @keydown.enter="update" @blur="update">
+        <input ref="input" v-model="newName" :disabled="!editMode" maxlength="30" spellcheck="false" type="text" @keydown.enter="update" @blur="() => {}">
         <div v-if="editMode" class="group-item-controls">
+          <ul class="group-item-swatches">
+            <li v-for="color in colors" class="group-item-swatch" @click="newColor = color">
+              <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" style="display: block;">
+                <circle cx="6" cy="6" r="6" :fill="color" />
+              </svg>
+            </li>
+          </ul>
           <button type="button" style="background: #fff; padding: 4px 6px;" @click="update">
             <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
               <path d="m6.25 10.6002-3.55-3.55-.7.7 3.55 3.55.7.7 7.05-7.05-.7-.75-6.35 6.4Z" fill="#606060" />

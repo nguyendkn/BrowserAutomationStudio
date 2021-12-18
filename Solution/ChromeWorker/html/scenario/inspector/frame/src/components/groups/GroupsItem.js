78a45306ae6e66320f86f1600174654b3c4f9583
CheshireCaat <likangt2012@gmail.com>
2021-12-18 06:04:01 +0300

    Update group on click outside

--- Solution/ChromeWorker/html/scenario/inspector/frame/src/components/groups/GroupsItem.js
+++ Solution/ChromeWorker/html/scenario/inspector/frame/src/components/groups/GroupsItem.js
@@ -56,6 +56,7 @@ window.GroupsItem = {
 
   methods: {
     update(cancel) {
+      if (!this.isEditing) return;
       this.$emit('update', this.id, {
         color: cancel ? (this.newColor = this.color) : this.newColor,
         name: cancel ? (this.newName = this.name) : this.newName,
@@ -89,33 +90,35 @@ window.GroupsItem = {
         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path d="M6 3.5v-2H0v12H16v-10H6Z" fill="#606060" stroke="#606060" />
         </svg>
-        <input ref="input" v-model="newName" :disabled="!isEditing" maxlength="30" spellcheck="false" type="text" @keydown.enter="accept" @keydown.esc="cancel">
-        <div v-if="isEditing" class="group-item-controls">
-          <ul class="group-item-swatches">
-            <li v-for="(value, key) in colors" class="group-item-swatch" :style="{ borderColor: newColor === key ? 'rgb(' + value + ')' : 'transparent' }" @click="newColor = key">
-              <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="6" cy="6" r="6" :fill="'rgb(' + value + ')'" />
+        <div class="group-item-inputs" v-click-outside="accept">
+          <input ref="input" v-model="newName" :disabled="!isEditing" maxlength="30" spellcheck="false" type="text" @keydown.enter="accept" @keydown.esc="cancel">
+          <div v-if="isEditing" class="group-item-controls">
+            <ul class="group-item-swatches">
+              <li v-for="(value, key) in colors" class="group-item-swatch" :style="{ borderColor: newColor === key ? 'rgb(' + value + ')' : 'transparent' }" @click="newColor = key">
+                <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="6" cy="6" r="6" :fill="'rgb(' + value + ')'" />
+                </svg>
+              </li>
+            </ul>
+            <button type="button" style="background: #fff; padding: 4px;" @click="accept">
+              <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="m6.25 10.6002-3.55-3.55-.7.7 3.55 3.55.7.7 7.05-7.05-.7-.75-6.35 6.4Z" fill="#606060" />
               </svg>
-            </li>
-          </ul>
-          <button type="button" style="background: #fff; padding: 4px;" @click="accept">
-            <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-              <path d="m6.25 10.6002-3.55-3.55-.7.7 3.55 3.55.7.7 7.05-7.05-.7-.75-6.35 6.4Z" fill="#606060" />
-            </svg>
-          </button>
-        </div>
-        <div v-else class="group-item-controls">
-          <template v-if="!primary">
-            <button type="button" @click="remove">
-              <icon-delete />
             </button>
-            <button type="button" @click="edit">
-              <icon-edit />
+          </div>
+          <div v-else class="group-item-controls">
+            <template v-if="!primary">
+              <button type="button" @click="remove">
+                <icon-delete />
+              </button>
+              <button type="button" @click="edit">
+                <icon-edit />
+              </button>
+            </template>
+            <button type="button" @click="isExpanded = !isExpanded">
+              <icon-chevron :style="{ transform: isExpanded ? 'rotate(180deg)' : '' }" />
             </button>
-          </template>
-          <button type="button" @click="isExpanded = !isExpanded">
-            <icon-chevron :style="{ transform: isExpanded ? 'rotate(180deg)' : '' }" />
-          </button>
+          </div>
         </div>
       </div>
       <draggable v-show="!isExpanded" class="group-item-content" :list="items" group="items" :disabled="true">

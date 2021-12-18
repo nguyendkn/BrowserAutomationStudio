5b4b43080a0ea0fb472aa4aa20d65b9e774a0946
CheshireCaat <likangt2012@gmail.com>
2021-12-19 02:28:56 +0300

    Update utilities

--- Solution/ChromeWorker/html/scenario/inspector/frame/src/utils/utils.js
+++ Solution/ChromeWorker/html/scenario/inspector/frame/src/utils/utils.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const scaleColor = (color, size = 6) => {
-  const scale = _.compose(color2K.parseToRgba, color2K.getScale('#ff0000', color));
-  return _.range(size).map(n => `rgb(${scale(n / (size - 1)).slice(0, -1).join(', ')})`);
-};
-
 const html = (raw, ...args) => String.raw({ raw }, args);
+
+const code = (raw, ...args) => String.raw({ raw }, args);
 
 const uniqueId = (() => {
   let counter = 0;
@@ -24,7 +21,6 @@ const getType = (() => {
 
     const type = typeof value;
     if (type !== 'object') return type;
-
     return toString.call(value).slice(8, -1).toLowerCase();
   };
 })();

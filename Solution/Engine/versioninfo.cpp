057ee20e01a64a00f445c85654794f1c32d0889a
twaego <mail.to.twaego@gmail.com>
2022-03-20 18:06:40 +0200

    Version 24.8.0

--- Solution/Engine/versioninfo.cpp
+++ Solution/Engine/versioninfo.cpp
@@ -22,12 +22,12 @@ namespace BrowserAutomationStudioFramework
 
     int VersionInfo::MinorVersion()
     {
-        return 7;
+        return 8;
     }
 
     int VersionInfo::BuildVersion()
     {
-        return 1;
+        return 0;
     }
 
     QString VersionInfo::VersionString()

131dca0bfc73ae9e23226c57c1b3ec24ff84acbf
GhostZ <sasha9817@mail.ru>
2022-05-19 18:25:36 +0400

    Improve description

--- Solution/Modules/InMail/js/InMail_Configure_interface.js
+++ Solution/Modules/InMail/js/InMail_Configure_interface.js
@@ -121,48 +121,50 @@
 			description: tr("The password of the mail server, it is almost always the same as the email password, but the mail service may have a separate password for access via imap and pop3 protocols.") + " " + tr("See the settings of the used protocol for your mail.")
 		}
 	}) %>
-	<%= _.template($('#input_constructor').html())({
-		id: "box",
-		description: tr("Folder name"),
-		default_selector: "string",
-		variants: [
-			{value: "INBOX", description: tr("Default folder incoming messages")}
-		],
-		disable_int: true,
-		value_string: "INBOX",
-		help: {
-			description: tr("The name of the folder in which all actions will be performed if no other folder is specified in their additional settings.") + " " + tr("You can get a list of mailbox folders using the \"Get list of folders\" action."),
-			examples: [
-				{code: "INBOX", description: tr("Default folder incoming messages")},
-				{code: "Spam", description: tr("Spam folder, on some mails")},
-				{code: "Trash", description: tr("Trash folder, on some mails")}
-			]
-		}
-	}) %>
-	<%= _.template($('#input_constructor').html())({
-		id: "connectTimeout",
-		description: tr("Connection timeout (seconds)"),
-		default_selector: "int",
-		disable_string: true,
-		value_number: 300,
-		min_number: 0,
-		max_number: 999999,
-		help: {
-			description: tr("The number of seconds after which the connection to the mail server will be considered unsuccessful.")
-		}
-	}) %>
-	<%= _.template($('#input_constructor').html())({
-		id: "timeout",
-		description: tr("Timeout (seconds)"),
-		default_selector: "int",
-		disable_string: true,
-		value_number: 300,
-		min_number: 0,
-		max_number: 999999,
-		help: {
-			description: tr("This action won't start connection, it only sets configuration. Connection will be established after first attempt to access server. When action, which triggers connection will be finished, connection won't be closed immediately.") + " " + tr("It will be preserved for a time specified in this parameter.") + " " + tr("If another action will require access to server, connection will be reused.")
-		}
-	}) %>
+	<%= _.template($('#block_start').html())({id:"Additional", name: tr("Additional settings"), description: ""}) %>
+		<%= _.template($('#input_constructor').html())({
+			id: "box",
+			description: tr("Folder name"),
+			default_selector: "string",
+			variants: [
+				{value: "INBOX", description: tr("Default folder incoming messages")}
+			],
+			disable_int: true,
+			value_string: "INBOX",
+			help: {
+				description: tr("The name of the folder in which all actions will be performed if no other folder is specified in their additional settings.") + " " + tr("You can get a list of mailbox folders using the \"Get list of folders\" action."),
+				examples: [
+					{code: "INBOX", description: tr("Default folder incoming messages")},
+					{code: "Spam", description: tr("Spam folder, on some mails")},
+					{code: "Trash", description: tr("Trash folder, on some mails")}
+				]
+			}
+		}) %>
+		<%= _.template($('#input_constructor').html())({
+			id: "connectTimeout",
+			description: tr("Connection timeout (seconds)"),
+			default_selector: "int",
+			disable_string: true,
+			value_number: 300,
+			min_number: 0,
+			max_number: 999999,
+			help: {
+				description: tr("The number of seconds after which the connection to the mail server will be considered unsuccessful.")
+			}
+		}) %>
+		<%= _.template($('#input_constructor').html())({
+			id: "timeout",
+			description: tr("Reset timeout (seconds)"),
+			default_selector: "int",
+			disable_string: true,
+			value_number: 300,
+			min_number: 0,
+			max_number: 999999,
+			help: {
+				description: tr("This action won't start connection, it only sets configuration. Connection will be established after first attempt to access server. When action, which triggers connection will be finished, connection won't be closed immediately.") + " " + tr("It will be preserved for a time specified in this parameter.") + " " + tr("If another action will require access to server, connection will be reused.")
+			}
+		}) %>
+	<%= _.template($('#block_end').html())() %>
 </div>
 <div class="tooltipinternal">
 	<div class="tr tooltip-paragraph-first-fold">Configure access to the mail server to read email.</div>
@@ -175,9 +177,9 @@
 	<div class="tooltip-paragraph-fold"><span class="tr">For most mail services, you also need to allow imap/pop3 access in your account settings. Here are <a href="#" class="tr" onclick="BrowserAutomationStudio_OpenUrl('https://support.google.com/mail/answer/7126229?hl=en');return false">settings</a> for gmail.</span> <span class="tr">Also, the mail service may have a separate password for connecting via imap/pop3.</span></div>
 	<div class="tooltip-paragraph-fold"><span class="tr">Gmail also requires to</span> <a href="#" class="tr" onclick="BrowserAutomationStudio_OpenUrl('https://myaccount.google.com/lesssecureapps');return false">allow insecure applications</a> <span class="tr">in order to work with BAS</span>.</div>
 	<div class="tr tooltip-paragraph-fold">Mail.ru doesn't allow to make search in mailbox.</div>
-	<div class="tr tooltip-paragraph-fold">Mailbox can contain several folders, and you can work with messages from a specific folder by changing "Folder name" parameter. By default it is set to "INBOX", this folder contains incoming messages. Each mail service provides its own default names for different folders. For example, this is default name of gmail trash folder "[Gmail]/Trash" and spam folder is "[Gmail]/Spam". You can also create your own folders and use them in this action.</div>
+	<div class="tr tooltip-paragraph-fold">Mailbox can contain several folders, and you can work with messages from a specific folder by changing "Folder name" parameter located in the additional settings. By default it is set to "INBOX", this folder contains incoming messages. Each mail service provides its own default names for different folders. For example, this is default name of gmail trash folder "[Gmail]/Trash" and spam folder is "[Gmail]/Spam". You can also create your own folders and use them in this action.</div>
 	<div class="tr tooltip-paragraph-fold">You can get a list of mailbox folders using the "Get list of folders" action.</div>
-	<div class="tooltip-paragraph-fold"><span class="tr">This action won't start connection, it only sets configuration. Connection will be established after first attempt to access server. When action, which triggers connection will be finished, connection won't be closed immediately.</span> <span class="tr">It will be preserved for a time specified in "Timeout" parameter.</span> <span class="tr">If another action will require access to server, connection will be reused.</span></div>
+	<div class="tooltip-paragraph-fold"><span class="tr">This action won't start connection, it only sets configuration. Connection will be established after first attempt to access server. When action, which triggers connection will be finished, connection won't be closed immediately.</span> <span class="tr">It will be preserved for a time specified in "Reset timeout" parameter located in the additional settings.</span> <span class="tr">If another action will require access to server, connection will be reused.</span></div>
 	<div class="tr tooltip-paragraph-fold">You don't need to close connection explicitly, it will be closed automatically after timeout or when the script will stop.</div>
 	<div class="tr tooltip-paragraph-last-fold">If an error occurred while execute action, the thread will stop with fail message. If you want to continue thread, use "Ignore errors" action.</div>
 </div>

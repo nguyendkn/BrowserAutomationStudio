5f7c9a3c51cc1409bd8c58d444f77703f94f151f
GhostZ <sasha9817@mail.ru>
2022-05-24 20:36:40 +0400

    Normalize paddings

--- Solution/Modules/PhoneVerification/js/internal/SmsReg/countries.js
+++ Solution/Modules/PhoneVerification/js/internal/SmsReg/countries.js
@@ -1,10 +1,10 @@
 _SMS.SmsRegApi.prototype.getRawCountry = function(country){
 	var countries = {
 		"Any": "all",
-        "RU": "ru",
-        "KZ": "kz",
-        "UA": "ua",
-        "CN": "cn"
+		"RU": "ru",
+		"KZ": "kz",
+		"UA": "ua",
+		"CN": "cn"
 	};
 	return countries.hasOwnProperty(country) ? countries[country] : countries["Any"];
 };
